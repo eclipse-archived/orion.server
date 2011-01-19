462d816ebbed20942174c3a7207ca22d85ae69e7
John Arthorne <John_Arthorne@ca.ibm.com>
2011-01-19 16:53:49 -0500

    Add import/export links to child directories

--- bundles/org.eclipse.orion.server.servlets/src/org/eclipse/orion/internal/server/servlets/xfer/TransferResourceDecorator.java
+++ bundles/org.eclipse.orion.server.servlets/src/org/eclipse/orion/internal/server/servlets/xfer/TransferResourceDecorator.java
@@ -11,12 +11,13 @@
 package org.eclipse.orion.internal.server.servlets.xfer;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.orion.internal.server.core.IWebResourceDecorator;
 import org.eclipse.orion.internal.server.servlets.ProtocolConstants;
 import org.eclipse.orion.server.core.LogHelper;
-import org.json.JSONObject;
+import org.json.*;
 
 /**
  * Adds links to the import and export services for files in the workspace.
@@ -31,18 +32,31 @@ public class TransferResourceDecorator implements IWebResourceDecorator {
 		if (targetPath.segmentCount() > 1 && "file".equals(targetPath.segment(0))) { //$NON-NLS-1$
 			targetPath = targetPath.removeFirstSegments(1);
 			try {
-				IPath path = new Path("/xfer").append(targetPath); //$NON-NLS-1$
-				URI link = new URI(resource.getScheme(), resource.getAuthority(), path.toString(), null, null);
-				representation.put(ProtocolConstants.KEY_IMPORT_LOCATION, link.toString());
-
-				path = new Path("/xfer/export").append(targetPath).addFileExtension("zip"); //$NON-NLS-1$ //$NON-NLS-2$
-				link = new URI(resource.getScheme(), resource.getAuthority(), path.toString(), null, null);
-				representation.put(ProtocolConstants.KEY_EXPORT_LOCATION, link.toString());
+				addTransferLinks(resource, representation, targetPath);
+				JSONArray children = representation.optJSONArray(ProtocolConstants.KEY_CHILDREN);
+				if (children != null) {
+					for (int i = 0; i < children.length(); i++) {
+						JSONObject child = children.getJSONObject(i);
+						if (child.getBoolean(ProtocolConstants.KEY_DIRECTORY)) {
+							IPath childPath = targetPath.append(child.getString(ProtocolConstants.KEY_NAME));
+							addTransferLinks(resource, child, childPath);
+						}
+					}
+				}
 			} catch (Exception e) {
 				//log and continue
 				LogHelper.log(e);
 			}
 		}
+	}
+
+	private void addTransferLinks(URI resource, JSONObject representation, IPath targetPath) throws URISyntaxException, JSONException {
+		IPath path = new Path("/xfer").append(targetPath); //$NON-NLS-1$
+		URI link = new URI(resource.getScheme(), resource.getAuthority(), path.toString(), null, null);
+		representation.put(ProtocolConstants.KEY_IMPORT_LOCATION, link.toString());
+		path = new Path("/xfer/export").append(targetPath).addFileExtension("zip"); //$NON-NLS-1$ //$NON-NLS-2$
+		link = new URI(resource.getScheme(), resource.getAuthority(), path.toString(), null, null);
+		representation.put(ProtocolConstants.KEY_EXPORT_LOCATION, link.toString());
 	}
 
 }
