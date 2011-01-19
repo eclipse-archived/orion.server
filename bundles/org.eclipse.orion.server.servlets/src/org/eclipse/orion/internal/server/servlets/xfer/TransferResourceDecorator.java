66e0a1cfb26529edcb443f106bf89d70bdb91599
John Arthorne <John_Arthorne@ca.ibm.com>
2011-01-19 17:21:12 -0500

    added import/export links for workspace resources

--- bundles/org.eclipse.orion.server.servlets/src/org/eclipse/orion/internal/server/servlets/xfer/TransferResourceDecorator.java
+++ bundles/org.eclipse.orion.server.servlets/src/org/eclipse/orion/internal/server/servlets/xfer/TransferResourceDecorator.java
@@ -29,28 +29,33 @@ public class TransferResourceDecorator implements IWebResourceDecorator {
 	 */
 	public void addAtributesFor(URI resource, JSONObject representation) {
 		IPath targetPath = new Path(resource.getPath());
-		if (targetPath.segmentCount() > 1 && "file".equals(targetPath.segment(0))) { //$NON-NLS-1$
-			targetPath = targetPath.removeFirstSegments(1);
-			try {
-				addTransferLinks(resource, representation, targetPath);
-				JSONArray children = representation.optJSONArray(ProtocolConstants.KEY_CHILDREN);
-				if (children != null) {
-					for (int i = 0; i < children.length(); i++) {
-						JSONObject child = children.getJSONObject(i);
-						if (child.getBoolean(ProtocolConstants.KEY_DIRECTORY)) {
-							IPath childPath = targetPath.append(child.getString(ProtocolConstants.KEY_NAME));
-							addTransferLinks(resource, child, childPath);
-						}
+		if (targetPath.segmentCount() <= 1)
+			return;
+		String servlet = targetPath.segment(0);
+		if (!"file".equals(servlet) && !"workspace".equals(servlet))
+			return;
+		try {
+			//don't add import/export directly on a workspace at this point
+			if ("file".equals(servlet))
+				addTransferLinks(resource, representation);
+			JSONArray children = representation.optJSONArray(ProtocolConstants.KEY_CHILDREN);
+			if (children != null) {
+				for (int i = 0; i < children.length(); i++) {
+					JSONObject child = children.getJSONObject(i);
+					if (child.getBoolean(ProtocolConstants.KEY_DIRECTORY)) {
+						addTransferLinks(resource, child);
 					}
 				}
-			} catch (Exception e) {
-				//log and continue
-				LogHelper.log(e);
 			}
+		} catch (Exception e) {
+			//log and continue
+			LogHelper.log(e);
 		}
 	}
 
-	private void addTransferLinks(URI resource, JSONObject representation, IPath targetPath) throws URISyntaxException, JSONException {
+	private void addTransferLinks(URI resource, JSONObject representation) throws URISyntaxException, JSONException {
+		URI location = new URI(representation.getString(ProtocolConstants.KEY_LOCATION));
+		IPath targetPath = new Path(location.getPath()).removeFirstSegments(1);
 		IPath path = new Path("/xfer").append(targetPath); //$NON-NLS-1$
 		URI link = new URI(resource.getScheme(), resource.getAuthority(), path.toString(), null, null);
 		representation.put(ProtocolConstants.KEY_IMPORT_LOCATION, link.toString());
