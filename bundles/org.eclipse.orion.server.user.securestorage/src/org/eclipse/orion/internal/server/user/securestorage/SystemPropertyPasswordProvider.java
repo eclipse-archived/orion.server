199cfea6be27116cf6c1ee07556a6d0a159d82fa
John Arthorne <John_Arthorne@ca.ibm.com>
2011-01-24 16:40:41 -0500

    Bug 334775 - [server] Implement User Service on Equinox Secure Storage

---
+++ bundles/org.eclipse.orion.server.user.securestorage/src/org/eclipse/orion/internal/server/user/securestorage/SystemPropertyPasswordProvider.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.orion.internal.server.user.securestorage;
+
+import javax.crypto.spec.PBEKeySpec;
+import org.eclipse.equinox.security.storage.provider.IPreferencesContainer;
+
+import org.eclipse.equinox.security.storage.provider.PasswordProvider;
+
+/**
+ * A default password provider that obtains a password from a system property.
+ */
+public class SystemPropertyPasswordProvider extends PasswordProvider {
+
+	@Override
+	public PBEKeySpec getPassword(IPreferencesContainer container, int passwordType) {
+		String prop = System.getProperty("orion.storage.password", ""); //$NON-NLS-1$ //$NON-NLS-2$
+		return new PBEKeySpec(prop.toCharArray());
+	}
+
+}
