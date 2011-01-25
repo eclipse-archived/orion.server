7aafba39b73bc51199a480478da0d40211ac9a21
Mark Macdonald <mamacdon@ca.ibm.com>
2011-01-25 17:53:19 -0500

    Bug 334956 - Simple Firebug integration

---
+++ bundles/org.eclipse.orion.server.servlets/src/org/eclipse/orion/internal/server/servlets/file/EditSupportFilter.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2010,2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.orion.internal.server.servlets.file;
+
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Adds headers giving the edit-server and edit-token for resources in the file system.
+ */
+public class EditSupportFilter implements Filter {
+
+	private static final String FILE_SERVLET_ALIAS = "/file"; //$NON-NLS-1$
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		String requestURI = httpRequest.getRequestURI();
+
+		if (requestURI.startsWith(FILE_SERVLET_ALIAS)) { //$NON-NLS-1$
+			String host = getHost(httpRequest);
+			httpResponse.addHeader("X-Edit-Server", host + "/coding.html#"); //$NON-NLS-1$ //$NON-NLS-2$
+			httpResponse.addHeader("X-Edit-Token", requestURI); //$NON-NLS-1$
+		} else {
+			String selfHostPath = System.getProperty("org.eclipse.orion.server.core.selfHostPath"); //$NON-NLS-1$
+			if (selfHostPath != null) {
+				String host = getHost(httpRequest);
+				httpResponse.addHeader("X-Edit-Server", host + "/coding.html#"); //$NON-NLS-1$ //$NON-NLS-2$
+				httpResponse.addHeader("X-Edit-Token", FILE_SERVLET_ALIAS + selfHostPath + requestURI); //$NON-NLS-1$
+			}
+		}
+
+		chain.doFilter(request, response);
+	}
+
+	private static String getHost(ServletRequest request) {
+		return request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort(); //$NON-NLS-1$ //$NON-NLS-2$;
+	}
+
+	public void destroy() {
+	}
+}
