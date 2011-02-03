c94aa6e57405c3621595fd576723a9ed7cf4cd2d
John Arthorne <John_Arthorne@ca.ibm.com>
2011-02-03 15:24:30 -0500

    Fixed project location in index to have trailing slash

--- bundles/org.eclipse.orion.server.search/src/org/eclipse/orion/internal/server/search/SearchActivator.java
+++ bundles/org.eclipse.orion.server.search/src/org/eclipse/orion/internal/server/search/SearchActivator.java
@@ -33,7 +33,7 @@ public class SearchActivator implements BundleActivator, IWebResourceDecorator {
 	 * version should be incremented whenever there are breaking changes to the
 	 * indexing schema or format.
 	 */
-	private static final int CURRENT_INDEX_GENERATION = 5;
+	private static final int CURRENT_INDEX_GENERATION = 6;
 
 	public static final boolean DEBUG = true;
 	private static final String INDEX_GENERATION_FILE = "index.generation";//$NON-NLS-1$
