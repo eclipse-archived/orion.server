395858cff2551ca7e81e2100f9d4ae5f46fec028
Malgorzata Janczarska <malgorzata.tomczyk@pl.ibm.com>
2011-02-03 17:36:43 +0100

    Bug 336070 [server] NPE on anonymous attempt to create users.

--- bundles/org.eclipse.orion.server.authentication.formopenid/src/org/eclipse/orion/server/authentication/formopenid/servlets/FormOpenIdLoginServlet.java
+++ bundles/org.eclipse.orion.server.authentication.formopenid/src/org/eclipse/orion/server/authentication/formopenid/servlets/FormOpenIdLoginServlet.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 IBM Corporation and others.
+ * Copyright (c) 2010, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,17 +10,6 @@
  *******************************************************************************/
 package org.eclipse.orion.server.authentication.formopenid.servlets;
 
-import org.eclipse.orion.server.core.resources.Base64;
-
-import org.eclipse.orion.server.openid.core.OpenIdHelper;
-import org.eclipse.orion.server.openid.core.OpenidConsumer;
-
-import org.eclipse.orion.server.authentication.formopenid.FormOpenIdAuthenticationService;
-
-import org.eclipse.orion.server.authentication.form.core.FormAuthHelper;
-
-import org.eclipse.orion.server.servlets.OrionServlet;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -29,6 +18,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.orion.server.authentication.form.core.FormAuthHelper;
+import org.eclipse.orion.server.authentication.formopenid.FormOpenIdAuthenticationService;
+import org.eclipse.orion.server.core.LogHelper;
+import org.eclipse.orion.server.core.resources.Base64;
+import org.eclipse.orion.server.openid.core.OpenIdHelper;
+import org.eclipse.orion.server.openid.core.OpenidConsumer;
+import org.eclipse.orion.server.servlets.OrionServlet;
+import org.eclipse.orion.server.useradmin.UnsupportedUserStoreException;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.osgi.framework.Version;
@@ -54,39 +51,44 @@ public class FormOpenIdLoginServlet extends OrionServlet {
 		String pathInfo = req.getPathInfo() == null ? "" : req.getPathInfo(); //$NON-NLS-1$
 
 		if (pathInfo.startsWith("/form")) { //$NON-NLS-1$
-			if (FormAuthHelper.performAuthentication(req, resp)) {
-				if (req.getParameter(OpenIdHelper.REDIRECT) != null && !req.getParameter(OpenIdHelper.REDIRECT).equals("")) { //$NON-NLS-1$
-					resp.sendRedirect(req.getParameter(OpenIdHelper.REDIRECT));
+			try {
+				if (FormAuthHelper.performAuthentication(req, resp)) {
+					if (req.getParameter(OpenIdHelper.REDIRECT) != null && !req.getParameter(OpenIdHelper.REDIRECT).equals("")) { //$NON-NLS-1$
+						resp.sendRedirect(req.getParameter(OpenIdHelper.REDIRECT));
+					} else {
+						resp.flushBuffer();
+					}
 				} else {
+					// redirection from
+					// FormAuthenticationService.setNotAuthenticated
+					String versionString = req.getHeader("Orion-Version"); //$NON-NLS-1$
+					Version version = versionString == null ? null : new Version(versionString);
+
+					// TODO: This is a workaround for calls
+					// that does not include the WebEclipse version header
+					String xRequestedWith = req.getHeader("X-Requested-With"); //$NON-NLS-1$
+
+					String invalidLoginError = "Invalid user or password";
+
+					if (version == null && !"XMLHttpRequest".equals(xRequestedWith)) { //$NON-NLS-1$
+						RequestDispatcher rd = req.getRequestDispatcher("/mixlogin?error=" + new String(Base64.encode(invalidLoginError.getBytes()))); //$NON-NLS-1$
+						rd.include(req, resp);
+					} else {
+						PrintWriter writer = resp.getWriter();
+						JSONObject jsonError = new JSONObject();
+						try {
+							jsonError.put("error", invalidLoginError); //$NON-NLS-1$
+							writer.print(jsonError);
+							resp.setContentType("application/json"); //$NON-NLS-1$
+						} catch (JSONException e) {/* ignore */
+						}
+						resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+					}
 					resp.flushBuffer();
 				}
-			} else {
-				// redirection from
-				// FormAuthenticationService.setNotAuthenticated
-				String versionString = req.getHeader("Orion-Version"); //$NON-NLS-1$
-				Version version = versionString == null ? null : new Version(versionString);
-
-				// TODO: This is a workaround for calls
-				// that does not include the WebEclipse version header
-				String xRequestedWith = req.getHeader("X-Requested-With"); //$NON-NLS-1$
-
-				String invalidLoginError = "Invalid user or password";
-
-				if (version == null && !"XMLHttpRequest".equals(xRequestedWith)) { //$NON-NLS-1$
-					RequestDispatcher rd = req.getRequestDispatcher("/mixlogin?error=" + new String(Base64.encode(invalidLoginError.getBytes()))); //$NON-NLS-1$
-					rd.include(req, resp);
-				} else {
-					PrintWriter writer = resp.getWriter();
-					JSONObject jsonError = new JSONObject();
-					try {
-						jsonError.put("error", invalidLoginError); //$NON-NLS-1$
-						writer.print(jsonError);
-						resp.setContentType("application/json"); //$NON-NLS-1$
-					} catch (JSONException e) {/* ignore */
-					}
-					resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-				}
-				resp.flushBuffer();
+			} catch (UnsupportedUserStoreException e) {
+				LogHelper.log(e);
+				resp.sendError(HttpServletResponse.SC_NOT_FOUND, e.getMessage());
 			}
 			return;
 		}
