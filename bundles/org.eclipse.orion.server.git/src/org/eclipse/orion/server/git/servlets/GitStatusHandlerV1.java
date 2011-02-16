2a00ccbaf4d09511b6c473c094d42154bb1f2fd6
Szymon Brandys <Szymon.Brandys@pl.ibm.com>
2011-02-16 15:06:26 +0100

    GitStatusHandlerV1 added

---
+++ bundles/org.eclipse.orion.server.git/src/org/eclipse/orion/server/git/servlets/GitStatusHandlerV1.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.orion.server.git.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.orion.internal.server.servlets.ServletResourceHandler;
+
+/**
+ * A handler for Git Status operation.
+ */
+public class GitStatusHandlerV1 extends ServletResourceHandler<String> {
+
+	private ServletResourceHandler<IStatus> statusHandler;
+
+	GitStatusHandlerV1(ServletResourceHandler<IStatus> statusHandler) {
+		this.statusHandler = statusHandler;
+	}
+
+	@Override
+	public boolean handleRequest(HttpServletRequest request,
+			HttpServletResponse response, String gitPathInfo)
+			throws ServletException {
+		return false;
+	}
+}
