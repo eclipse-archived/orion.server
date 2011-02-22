8624cab89c5e03005be591d6afb6e178975c18c2
Szymon Brandys <Szymon.Brandys@pl.ibm.com>
2011-02-22 12:30:54 +0100

    Bug 337807 - [server] Get user profile plug-in URLs from the user profile

--- bundles/org.eclipse.orion.server.git/src/org/eclipse/orion/server/git/GitUserDecorator.java
+++ bundles/org.eclipse.orion.server.git/src/org/eclipse/orion/server/git/GitUserDecorator.java
@@ -11,6 +11,7 @@
 package org.eclipse.orion.server.git;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,6 +20,7 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.orion.internal.server.core.IWebResourceDecorator;
 import org.eclipse.orion.server.core.LogHelper;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class GitUserDecorator implements IWebResourceDecorator {
@@ -27,26 +29,36 @@ public class GitUserDecorator implements IWebResourceDecorator {
 	public void addAtributesFor(HttpServletRequest request, URI resource,
 			JSONObject representation) {
 		IPath targetPath = new Path(resource.getPath());
-		if (targetPath.segmentCount() <= 1)
-			return;
 		String servlet = targetPath.segment(0);
 		if (!"users".equals(servlet))
 			return;
 
 		try {
-			JSONArray children = representation.optJSONArray("Plugins");
+			addPluginLinks(resource, representation);
+
+			JSONArray children = representation.optJSONArray("users");
 			if (children != null) {
-				JSONObject plugin = new JSONObject();
-				URI result = new URI(resource.getScheme(),
-						resource.getUserInfo(), resource.getHost(),
-						resource.getPort(),
-						"/plugins/git/userProfilePlugin.html", null, null); //$NON-NLS-1$
-				plugin.put("Url", result);
-				children.put(plugin);
+				for (int i = 0; i < children.length(); i++) {
+					JSONObject child = children.getJSONObject(i);
+					addPluginLinks(resource, child);
+				}
 			}
 		} catch (Exception e) {
 			// log and continue
 			LogHelper.log(e);
+		}
+	}
+
+	private void addPluginLinks(URI resource, JSONObject representation)
+			throws URISyntaxException, JSONException {
+		JSONArray plugins = representation.optJSONArray("Plugins");
+		if (plugins != null) {
+			JSONObject plugin = new JSONObject();
+			URI result = new URI(resource.getScheme(), resource.getUserInfo(),
+					resource.getHost(), resource.getPort(),
+					"/plugins/git/userProfilePlugin.html", null, null); //$NON-NLS-1$
+			plugin.put("Url", result);
+			plugins.put(plugin);
 		}
 	}
 }
