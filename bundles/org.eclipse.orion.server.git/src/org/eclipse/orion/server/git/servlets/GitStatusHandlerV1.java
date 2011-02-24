00f4ecf34a75cc10381dd31539d2ad3d6c991e14
Szymon Brandys <Szymon.Brandys@pl.ibm.com>
2011-02-24 16:30:03 +0100

    GitStatusHandlerV1 returns a fake JSON response now

--- bundles/org.eclipse.orion.server.git/src/org/eclipse/orion/server/git/servlets/GitStatusHandlerV1.java
+++ bundles/org.eclipse.orion.server.git/src/org/eclipse/orion/server/git/servlets/GitStatusHandlerV1.java
@@ -15,7 +15,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.URIUtil;
+import org.eclipse.orion.internal.server.servlets.ProtocolConstants;
+import org.eclipse.orion.internal.server.servlets.ServerStatus;
 import org.eclipse.orion.internal.server.servlets.ServletResourceHandler;
+import org.eclipse.orion.server.servlets.OrionServlet;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * A handler for Git Status operation.
@@ -32,7 +39,49 @@ public class GitStatusHandlerV1 extends ServletResourceHandler<String> {
 	public boolean handleRequest(HttpServletRequest request,
 			HttpServletResponse response, String gitPathInfo)
 			throws ServletException {
-		// TODO: implement
-		return false;
+		try {
+			JSONObject result = new JSONObject();
+			createStatusRepresentation(request, result, null);
+			OrionServlet.writeJSONResponse(request, response, result);
+			return true;
+		} catch (JSONException e) {
+			return statusHandler.handleRequest(request, response,
+					new ServerStatus(IStatus.ERROR,
+							HttpServletResponse.SC_BAD_REQUEST,
+							"Syntax error in request", e));
+		} catch (Exception e) {
+			throw new ServletException("Error creating Git status", e);
+		}
+	}
+
+	private void createStatusRepresentation(HttpServletRequest request,
+			JSONObject representation, String path) throws JSONException {
+		representation.put(ProtocolConstants.KEY_LOCATION, URIUtil.append(
+				OrionServlet.getURI(request), path == null ? "" : path));
+
+		JSONObject file = new JSONObject();
+		createFakeFileRepresentation(request, file);
+		representation.put("File", file);
+
+		if (path == null) {
+			JSONArray children = new JSONArray();
+			generateFakeChildren(request, children);
+			representation.put(ProtocolConstants.KEY_CHILDREN, children);
+		}
+	}
+
+	private void createFakeFileRepresentation(HttpServletRequest request,
+			JSONObject representation) throws JSONException {
+		representation.put(ProtocolConstants.KEY_NAME, "SomeName");
+		representation.put(ProtocolConstants.KEY_LOCATION, "SomeLocation");
+	}
+
+	private void generateFakeChildren(HttpServletRequest request,
+			JSONArray representation) throws JSONException {
+		for (int i = 0; i < 3; i++) {
+			JSONObject child = new JSONObject();
+			createStatusRepresentation(request, child, "path" + i);
+			representation.put(child);
+		}
 	}
 }
