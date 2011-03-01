eclipse-archived/orion.server

commit 337363a07b909fbb288cc7948a99c39dca67c045

Bug 337443 - [client] consolidating login info in the UI

bundles/org.eclipse.orion.server.authentication.form/static/js/xhrAuth.js
@@ -167,14 +167,10 @@ function checkUser() {
 		handleAs : "json",
 		timeout : 15000,
 		load : function(jsonData, ioArgs) {
-			var statusPane = dojo.byId("authStatusPane")||null;
-			if (statusPane!=null) {
 				var lastLogin = "N/A";
 				if (jsonData.lastlogintimestamp != null)
 					lastLogin = dojo.date.locale.format(new Date(jsonData.lastlogintimestamp), {formatLength: "short"});
-				dojo.byId("authStatusPane").innerHTML = jsonData.login + " signed in at: " + lastLogin;
-				document.getElementById("signOutUser").innerHTML = "Sign out";
-				document.getElementById("signOutUser").onclick = logout;
+				eclipse.globalCommandUtils.generateUserInfo(jsonData.login, "logged in since " + lastLogin);
 			}
 			return jsonData;
 		},
@@ -195,12 +191,7 @@ function logout() {
 		handleAs : "json",
 		timeout : 15000,
 		load : function(jsonData, ioArgs) {
-			var statusPane = dojo.byId("authStatusPane")||null;
-			if (statusPane!=null) {
-				dojo.byId("authStatusPane").innerHTML = "--";
-				document.getElementById("signOutUser").innerHTML = "Sign in";
-				document.getElementById("signOutUser").onclick=login;
-			}
+			eclipse.globalCommandUtils.generateUserInfo();
 			window.location.replace("/index.html");
 			return jsonData;
 		},

bundles/org.eclipse.orion.server.authentication.formopenid/static/js/xhrAuth.js
@@ -193,15 +193,10 @@ function checkUser() {
 		handleAs : "json",
 		timeout : 15000,
 		load : function(jsonData, ioArgs) {
-			var statusPane = dojo.byId("authStatusPane")||null;
-			if (statusPane!=null) {
-				var lastLogin = "N/A";
-				if (jsonData.lastlogintimestamp != null)
-					lastLogin = dojo.date.locale.format(new Date(jsonData.lastlogintimestamp), {formatLength: "short"});
-				dojo.byId("authStatusPane").innerHTML = jsonData.login + " signed in at: " + lastLogin;
-				document.getElementById("signOutUser").innerHTML = "Sign out";
-				document.getElementById("signOutUser").onclick = logout;
-			}
+			var lastLogin = "N/A";
+			if (jsonData.lastlogintimestamp != null)
+				lastLogin = dojo.date.locale.format(new Date(jsonData.lastlogintimestamp), {formatLength: "short"});
+			eclipse.globalCommandUtils.generateUserInfo(jsonData.login, "logged in since " + lastLogin);
 			return jsonData;
 		},
 		error : function(response, ioArgs) {
@@ -221,12 +216,7 @@ function logout() {
 		handleAs : "json",
 		timeout : 15000,
 		load : function(jsonData, ioArgs) {
-			var statusPane = dojo.byId("authStatusPane")||null;
-			if (statusPane!=null) {
-				dojo.byId("authStatusPane").innerHTML = "--";
-				document.getElementById("signOutUser").innerHTML = "Sign in";
-				document.getElementById("signOutUser").onclick = login;
-			}
+			eclipse.globalCommandUtils.generateUserInfo();
 			window.location.replace("/index.html");
 			return jsonData;
 		},

bundles/org.eclipse.orion.server.authentication.openid/static/js/xhrAuth.js
@@ -116,11 +116,10 @@ function checkUser() {
 		handleAs : "json",
 		timeout : 15000,
 		load : function(jsonData, ioArgs) {
-			if (dojo.byId("authStatusPane") !== null) {
-				dojo.byId("authStatusPane").innerHTML = jsonData.login;
-				document.getElementById("signOutUser").innerHTML = "Sign out";
-				document.getElementById("signOutUser").onclick = logout;
-			}
+			var lastLogin = "N/A";
+			if (jsonData.lastlogintimestamp != null)
+				lastLogin = dojo.date.locale.format(new Date(jsonData.lastlogintimestamp), {formatLength: "short"});
+			eclipse.globalCommandUtils.generateUserInfo(jsonData.login, "logged in since " + lastLogin);
 			return jsonData;
 		},
 		error : function(response, ioArgs) {
@@ -140,11 +139,7 @@ function logout() {
 		handleAs : "json",
 		timeout : 15000,
 		load : function(jsonData, ioArgs) {
-			if (dojo.byId("authStatusPane") !== null) {
-				dojo.byId("authStatusPane").innerHTML = "--";
-				document.getElementById("signOutUser").innerHTML = "Sign in";
-				document.getElementById("signOutUser").onclick = login;
-			}
+			eclipse.globalCommandUtils.generateUserInfo();
 			window.location.replace("/index.html");
 			return jsonData;
 		},

bundles/org.eclipse.orion.server.authentication.form/static/js/loadXhrAuth.js
@@ -23,26 +23,5 @@ if (document.getElementById("formAuth") == null) {
 	dojo.byId('loginWindow').style.visibility = 'hidden';
 	dojo.byId('loginWindowMask').style.visibility = 'hidden';
 
-	// add the logout button to the toolbar if available
-	var toolBar = dojo.byId('eclipseWebToolbar');
-	if (toolBar) {
-		var signout = document.createElement('span');
-		signout.appendChild(document.createTextNode("Sign in"));
-		signout.onclick = function() {
-			login();
-		};
-		signout.id = "signOutUser";
-		toolBar.appendChild(signout);
-		dojo.addClass(signout, "commandLink");
-		var bElement = document.createElement('b');
-		bElement.setAttribute('id', 'authStatusPane');
-		bElement.innerHTML = '--';
-		if (dojo.isIE) {
-			bElement.setAttribute('className', 'statuspane');
-		} else {
-			bElement.setAttribute('class', 'statuspane');
-		}
-
-		toolBar.appendChild(bElement);
-	}
+	eclipse.globalCommandUtils.generateUserInfo();
 }

bundles/org.eclipse.orion.server.authentication.formopenid/static/js/loadXhrAuth.js
@@ -23,26 +23,5 @@ if (document.getElementById("formAuth") == null) {
 	dojo.byId('loginWindow').style.visibility = 'hidden';
 	dojo.byId('loginWindowMask').style.visibility = 'hidden';
 
-	// add the logout button to the toolbar if available
-	var toolBar = dojo.byId('eclipseWebToolbar');
-	if (toolBar) {
-		var signout = document.createElement('span');
-		signout.appendChild(document.createTextNode("Sign in"));
-		signout.onclick = function() {
-			login();
-		};
-		signout.id = "signOutUser";
-		dojo.addClass(signout, "commandLink");
-		toolBar.appendChild(signout);
-		var bElement = document.createElement('b');
-		bElement.setAttribute('id', 'authStatusPane');
-		bElement.innerHTML = '--';
-		if (dojo.isIE) {
-			bElement.setAttribute('className', 'statuspane');
-		} else {
-			bElement.setAttribute('class', 'statuspane');
-		}
-
-		toolBar.appendChild(bElement);
-	}
+	eclipse.globalCommandUtils.generateUserInfo();
 }

bundles/org.eclipse.orion.server.authentication.openid/static/js/loadXhrAuth.js
@@ -22,26 +22,6 @@ if (document.getElementById("openIdAuth") == null) {
 	dojo.byId('loginWindow').style.visibility = 'hidden';
 	dojo.byId('loginWindowMask').style.visibility = 'hidden';
 
-	// add the logout button to the toolbar if available
-	var toolBar = dojo.byId('eclipseWebToolbar');
-	if (toolBar) {
-		var signout = document.createElement('span');
-		signout.appendChild(document.createTextNode("Sign in"));
-		signout.onclick = function() {
-			login();
-		};
-		signout.id = "signOutUser";
-		dojo.addClass(signout, "commandLink");
-		toolBar.appendChild(signout);
-		var bElement = document.createElement('b');
-		bElement.setAttribute('id', 'authStatusPane');
-		bElement.innerHTML = '--';
-		if (dojo.isIE) {
-			bElement.setAttribute('className', 'statuspane');
-		} else {
-			bElement.setAttribute('class', 'statuspane');
-		}
+	eclipse.globalCommandUtils.generateUserInfo();
 
-		toolBar.appendChild(bElement);
-	}
 }

commit dbc262391a56ce61570b384390db8edbbb7cb26b

fixed syntax error introduced by 337363a0

bundles/org.eclipse.orion.server.authentication.form/static/js/xhrAuth.js
@@ -171,7 +171,6 @@ function checkUser() {
 				if (jsonData.lastlogintimestamp != null)
 					lastLogin = dojo.date.locale.format(new Date(jsonData.lastlogintimestamp), {formatLength: "short"});
 				eclipse.globalCommandUtils.generateUserInfo(jsonData.login, "logged in since " + lastLogin);
-			}
 			return jsonData;
 		},
 		error : function(response, ioArgs) {