4757043e317e3df71fcc4c1b65b35df42253504c
Malgorzata Janczarska <malgorzata.tomczyk@pl.ibm.com>
2011-04-18 11:47:20 +0200

    Bug 336822 [client] Add a proper prompt for new hosts accessed via ssh

---
+++ bundles/org.eclipse.orion.server.git/src/org/eclipse/orion/server/jsch/LazyKnownHosts.java
@@ -0,0 +1,101 @@
+/*******************************************************************************
+ * Copyright (c) 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.orion.server.jsch;
+
+import com.jcraft.jsch.*;
+import java.io.*;
+
+/**
+ * Use this repository instead of standard {@link HostKeyRepository} to record the last checked host fingerprint.
+ * If the last status recorded as {@link #lastStatus}, if check in the repository is different than {@link HostKeyRepository#OK}
+ * the host and its key are recorded as {@link #lastUnknownHost} and {@link #lastUnknownKey}.
+ *
+ */
+public class LazyKnownHosts implements HostKeyRepository {
+
+	private HostKeyRepository repo;
+
+	private String lastUnknownHost = null;
+	private byte[] lastUnknownKey = null;
+	private int lastStatus = OK;
+
+	LazyKnownHosts(JSch jsch, String knownHosts) throws JSchException {
+		try {
+			final InputStream in = new StringBufferInputStream(knownHosts);
+			try {
+				jsch.setKnownHosts(in);
+			} finally {
+				in.close();
+			}
+		} catch (IOException e) {
+			// no known hosts
+		}
+		this.repo = jsch.getHostKeyRepository();
+
+	}
+
+	@Override
+	public int check(String host, byte[] key) {
+		lastStatus = repo.check(host, key);
+
+		if (lastStatus != OK) {
+			lastUnknownHost = host;
+			lastUnknownKey = key;
+		} else {
+			lastUnknownHost = null;
+			lastUnknownKey = null;
+		}
+		return lastStatus;
+	}
+
+	@Override
+	public void add(HostKey hostkey, UserInfo ui) {
+		repo.add(hostkey, ui);
+	}
+
+	@Override
+	public void remove(String host, String type) {
+		repo.remove(host, type);
+	}
+
+	@Override
+	public void remove(String host, String type, byte[] key) {
+		repo.remove(host, type, key);
+	}
+
+	@Override
+	public String getKnownHostsRepositoryID() {
+		return "LAZY_" + repo.getKnownHostsRepositoryID();
+	}
+
+	@Override
+	public HostKey[] getHostKey() {
+		return repo.getHostKey();
+	}
+
+	@Override
+	public HostKey[] getHostKey(String host, String type) {
+		return repo.getHostKey();
+	}
+
+	public String getLastUnknownkedHost() {
+		return lastUnknownHost;
+	}
+
+	public byte[] getLastUnknownKey() {
+		return lastUnknownKey;
+	}
+
+	public int getLastStatus() {
+		return lastStatus;
+	}
+
+}
