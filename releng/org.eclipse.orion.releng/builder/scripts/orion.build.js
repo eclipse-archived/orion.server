3e45aa2c2306bff6d667951a7ffed7bc55f4b6f3
Andrew Niefer <aniefer@ca.ibm.com>
2011-05-31 09:55:30 -0400

    optimize with requirejs

--- releng/org.eclipse.orion.releng/builder/scripts/orion.build.js
+++ releng/org.eclipse.orion.releng/builder/scripts/orion.build.js
@@ -1,4 +1,43 @@
 ({
-    optimize: "none",
-    optimizeCss: "standard.keepLines"
+    optimizeCss: "standard.keepLines",
+    
+	closure: {
+		CompilerOptions: {},
+		CompilationLevel: 'SIMPLE_OPTIMIZATIONS',
+		loggingLevel: 'WARNING'
+	},
+    
+	pragmas: {
+		asynchLoader: true
+	},
+
+	locale: 'en-us',
+	inlineText: true,
+
+    packages: [
+        {
+	      name: 'dojo',
+	      location: '../org.dojotoolkit/dojo',
+	      main: 'lib/main-browser',
+	      lib: '.'
+	    },
+	    {
+	      name: 'dijit',
+	      location: '../org.dojotoolkit/dijit',
+	      main: 'lib/main',
+	      lib: '.'
+	    },
+	    {
+	      name: 'dojox',
+	      location: '../org.dojotoolkit/dojox',
+	      main: 'lib/main',
+	      lib: '.'
+	    }		    
+	],
+	  
+    paths: {
+    	orion: '../js',
+	    text: '../requirejs/text',
+	    i18n: '../requirejs/i18n'
+    },
 })
