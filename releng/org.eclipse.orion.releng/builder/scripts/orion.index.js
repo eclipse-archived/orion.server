15a16ee099366d57c2ff6949cafbc00a10c17a27
Andrew Niefer <aniefer@ca.ibm.com>
2011-06-02 17:35:07 -0400

    optimize index.html

---
+++ releng/org.eclipse.orion.releng/builder/scripts/orion.index.js
@@ -0,0 +1,43 @@
+({
+    optimizeCss: "standard.keepLines",
+    
+	closure: {
+		CompilerOptions: {},
+		CompilationLevel: 'SIMPLE_OPTIMIZATIONS',
+		loggingLevel: 'WARNING'
+	},
+    
+	pragmas: {
+		asynchLoader: true
+	},
+
+	locale: 'en-us',
+	inlineText: true,
+
+    packages: [
+        {
+	      name: 'dojo',
+	      location: 'org.dojotoolkit/dojo',
+	      main: 'lib/main-browser',
+	      lib: '.'
+	    },
+	    {
+	      name: 'dijit',
+	      location: 'org.dojotoolkit/dijit',
+	      main: 'lib/main',
+	      lib: '.'
+	    },
+	    {
+	      name: 'dojox',
+	      location: 'org.dojotoolkit/dojox',
+	      main: 'lib/main',
+	      lib: '.'
+	    }		    
+	],
+	  
+    paths: {
+    	orion: 'orion',
+	    text: 'requirejs/text',
+	    i18n: 'requirejs/i18n'
+    },
+})
