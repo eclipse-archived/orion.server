d6b2c197619931364480e59d370283677d652684
skaegi <simon_kaegi@ca.ibm.com>
2012-02-29 16:45:38 -0500

    Bug 372918 - WelcomeFileFilter should only activate when there is no valid content

--- bundles/org.eclipse.orion.server.configurator/src/org/eclipse/orion/server/configurator/servlet/WelcomeFileFilter.java
+++ bundles/org.eclipse.orion.server.configurator/src/org/eclipse/orion/server/configurator/servlet/WelcomeFileFilter.java
@@ -11,10 +11,8 @@
 package org.eclipse.orion.server.configurator.servlet;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.*;
 
 /**
  * A filter that forwards directory access to the index.html for that directory.
@@ -22,31 +20,57 @@ import javax.servlet.http.HttpServletRequest;
 public class WelcomeFileFilter implements Filter {
 
 	private static final String WELCOME_FILE_NAME = "index.html";//$NON-NLS-1$
-	private static final List<String> SERVLET_PATHS = Arrays.asList("/auth2", "/login", "/hosted", "/file", "/workspace/", "/filesystems", "/prefs", "/filesearch", "/git", "/users", "/site", "/xfer", "/help"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$ //$NON-NLS-9$ //$NON-NLS-10$
 
 	public void init(FilterConfig filterConfig) throws ServletException {
 		//nothing to do
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		String requestPath = httpRequest.getServletPath() + (httpRequest.getPathInfo() == null ? "" : httpRequest.getPathInfo());
+		final HttpServletRequest httpRequest = (HttpServletRequest) request;
+		final String requestPath = httpRequest.getServletPath() + (httpRequest.getPathInfo() == null ? "" : httpRequest.getPathInfo());
 		// Only alter directories that aren't part of our servlets
-		if (requestPath.endsWith("/") && !isServletPath(requestPath)) { //$NON-NLS-1$
-			request.getRequestDispatcher(requestPath + WELCOME_FILE_NAME).forward(request, response);
-			return;
+		if (requestPath.endsWith("/")) {
+			response = new HttpServletResponseWrapper((HttpServletResponse) response) {
+
+				private boolean handleWelcomeFile(int sc) {
+					if (sc == SC_NOT_FOUND || sc == SC_FORBIDDEN) {
+						try {
+							httpRequest.getRequestDispatcher(requestPath + WELCOME_FILE_NAME).forward(httpRequest, getResponse());
+							return true;
+						} catch (Exception e) {
+							// fall through
+						}
+					}
+					return false;
+				}
+
+				public void sendError(int sc) throws IOException {
+					if (!handleWelcomeFile(sc)) {
+						super.sendError(sc);
+					}
+				}
+
+				public void sendError(int sc, String msg) throws IOException {
+					if (!handleWelcomeFile(sc)) {
+						super.sendError(sc, msg);
+					}
+				}
+
+				public void setStatus(int sc) {
+					if (!handleWelcomeFile(sc)) {
+						super.setStatus(sc);
+					}
+				}
+
+				/**@deprecated*/
+				public void setStatus(int sc, String sm) {
+					if (!handleWelcomeFile(sc)) {
+						super.setStatus(sc, sm);
+					}
+				}
+			};
 		}
 		chain.doFilter(request, response);
-	}
-
-	/**
-	 * Returns whether the given request URI represents one of our registered servlets.
-	 */
-	private boolean isServletPath(String requestURI) {
-		for (String prefix : SERVLET_PATHS)
-			if (requestURI.startsWith(prefix))
-				return true;
-		return false;
 	}
 
 	public void destroy() {
