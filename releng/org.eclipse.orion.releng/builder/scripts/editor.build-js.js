0f0c92f90dcf8d745244ab70c912eff279a907e9
John Arthorne <John_Arthorne@ca.ibm.com>
2013-01-31 15:15:06 -0500

    Build editor js and css

---
+++ releng/org.eclipse.orion.releng/builder/scripts/editor.build-js.js
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+ 
+// optimization script to concat/minify the Orion editor javascript code
+ 
+({
+    baseUrl: '.',
+	closure: {
+		CompilerOptions: {},
+		CompilationLevel: 'SIMPLE_OPTIMIZATIONS',
+		loggingLevel: 'WARNING'
+	},
+	paths: {
+        i18n: 'requirejs/i18n',
+	},
+	name: "orion/editor/edit",
+	preserveLicenseComments: false,
+	uglify: {
+		ascii_only: true
+	},
+	wrap: {
+		start: "",
+		end: "\ndefine(['orion/editor/edit'], function(edit) {return edit;});"
+	},
+	out: "built-orioneditor.js"
+})
