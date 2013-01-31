b8fe16441f9b77b7f3cc8fd038557bc3163b4b0b
John Arthorne <John_Arthorne@ca.ibm.com>
2013-01-31 16:12:36 -0500

    Fixed editor optimize wrappers

--- releng/org.eclipse.orion.releng/builder/scripts/editor.build-js.js
+++ releng/org.eclipse.orion.releng/builder/scripts/editor.build-js.js
@@ -1,5 +1,4 @@
 /*******************************************************************************
- * @license
  * Copyright (c) 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials are made 
  * available under the terms of the Eclipse Public License v1.0 
@@ -29,7 +28,7 @@
 	},
 	wrap: {
 		start: "",
-		end: "\ndefine(['orion/editor/edit'], function(edit) {return edit;});"
+		end: "define(['orion/editor/edit'], function(edit) {return edit;});"
 	},
 	out: "built-orioneditor.js"
 })
