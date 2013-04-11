141b268e0edb5a72be2767a0978b03756fbbf009
John Arthorne <John_Arthorne@ca.ibm.com>
2013-04-11 12:08:58 -0400

    Initial work in progress on new backing store API

---
+++ bundles/org.eclipse.orion.server.core/src/org/eclipse/orion/server/core/metastore/MetadataInfo.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.orion.server.core.metastore;
+
+import java.util.*;
+
+/**
+ * A snapshot of information about an entity of the Orion metadata, such as a user,
+ * a workspace, or a project. The information in this structure is disconnected from any particular backing 
+ * store. Changes to this object are not persisted without invoking a method on @link {@link IMetaStore}.
+ */
+public class MetadataInfo {
+	private String id;
+	private final Map<String, String> properties = Collections.synchronizedMap(new HashMap<String, String>());
+
+	/**
+	 * Creates a new empty metadata object.
+	 */
+	public MetadataInfo() {
+		super();
+	}
+
+	/**
+	 * Returns the globally unique id of this metadata object.
+	 * @return the id of this object
+	 */
+	public String getUID() {
+		return id;
+	}
+
+	public void setProperty(String key, String value) {
+		properties.put(key, value);
+	}
+
+}
