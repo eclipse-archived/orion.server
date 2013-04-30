6d422ccf3e5fb0c2b022c62d77807b19436f8977
John Arthorne <John_Arthorne@ca.ibm.com>
2013-04-30 16:25:25 -0400

    Work in progress on new metastore

---
+++ bundles/org.eclipse.orion.server.core/src/org/eclipse/orion/internal/server/core/metastore/SimpleMetaStore.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.orion.internal.server.core.metastore;
+
+import java.util.List;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.orion.server.core.metastore.*;
+
+/**
+ * A simple metadata storage implementation that segments metadata by user
+ * and stores metadata alongside user's data files.
+ */
+public class SimpleMetaStore implements IMetaStore {
+	//	- workspace/
+	//		- prefix/
+	//			- userId/
+	//				- userId.txt -> User and workspace settings
+	//				- projectA/
+	//					- 
+
+	public void createProject(String workspaceId, ProjectInfo info) throws CoreException {
+	}
+
+	public void createUser(UserInfo info) throws CoreException {
+	}
+
+	public void createWorkspace(String userId, WorkspaceInfo info) throws CoreException {
+	}
+
+	public void deleteProject(String workspaceId, String projectName) throws CoreException {
+	}
+
+	public void deleteUser(String userId) throws CoreException {
+	}
+
+	public void deleteWorkspace(String userId, String workspaceId) throws CoreException {
+	}
+
+	public List<String> readAllUsers() throws CoreException {
+		return null;
+	}
+
+	public ProjectInfo readProject(String projectId) throws CoreException {
+		return null;
+	}
+
+	public ProjectInfo readProject(String workspaceId, String projectName) throws CoreException {
+		return null;
+	}
+
+	public UserInfo readUser(String userId) throws CoreException {
+		return null;
+	}
+
+	public WorkspaceInfo readWorkspace(String workspaceId) throws CoreException {
+		return null;
+	}
+
+	public void updateProject(ProjectInfo project) throws CoreException {
+	}
+
+	public void updateUser(UserInfo info) throws CoreException {
+	}
+
+}
