e5ad86bb3e886262aed11c74ecbf4d9bd905c50c
Anthony Hunter <anthonyh@ca.ibm.com>
2013-07-17 13:06:50 -0400

    Bug 412995 [server] provide simple metadata storage

--- tests/org.eclipse.orion.server.tests/src/org/eclipse/orion/server/tests/metastore/MetaStoreTests.java
+++ tests/org.eclipse.orion.server.tests/src/org/eclipse/orion/server/tests/metastore/MetaStoreTests.java
@@ -22,6 +22,7 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -29,76 +30,165 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
 import org.eclipse.orion.internal.server.servlets.ProtocolConstants;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class MetaStoreTests {
 
-	protected String orionUser;
+	protected static String orionTestName = null;
 
-	protected URI getOrionServerURI(String path) throws URISyntaxException {
-		//String orionServerHostname = "vottachrh6x64.ottawa.ibm.com";
-		String orionServerHostname = "localhost";
-		int orionServerPort = 8080;
-
-		return new URI("http", "", orionServerHostname, orionServerPort, path, "", "");
+	protected HttpClient createHttpClient() {
+		ThreadSafeClientConnManager cm = new ThreadSafeClientConnManager();
+		cm.setMaxTotal(100);
+		return new DefaultHttpClient(cm);
 	}
 
-	protected String getOrionUser() {
-		if (orionUser == null) {
-			orionUser = "test" + System.currentTimeMillis();
+	protected URI getOrionServerURI(String path) throws URISyntaxException {
+		String orionServerHostname = "vottachrh6x64.ottawa.ibm.com";
+		//String orionServerHostname = "localhost";
+		int orionServerPort = 8080;
+
+		return new URI("http", null, orionServerHostname, orionServerPort, path, null, null);
+	}
+
+	protected String getOrionTestName() {
+		if (orionTestName == null) {
+			orionTestName = "test" + System.currentTimeMillis();
 		}
-		return orionUser;
+		return orionTestName;
 	}
 
 	protected int login(HttpClient httpClient) throws ClientProtocolException, IOException, URISyntaxException {
 		HttpPost httpPost = new HttpPost(getOrionServerURI("/login/form"));
 		httpPost.setHeader(ProtocolConstants.HEADER_ORION_VERSION, "1");
 		List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-		nvps.add(new BasicNameValuePair("login", "test"));
-		nvps.add(new BasicNameValuePair("password", "test"));
+		nvps.add(new BasicNameValuePair("login", getOrionTestName()));
+		nvps.add(new BasicNameValuePair("password", getOrionTestName()));
 		httpPost.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"));
 
 		HttpResponse httpResponse = httpClient.execute(httpPost);
-		HttpEntity httpEntity = httpResponse.getEntity();
-		if (httpEntity != null) {
-			String result = EntityUtils.toString(httpEntity);
-			System.out.println("- " + result);
-		}
+		EntityUtils.consume(httpResponse.getEntity());
 		return httpResponse.getStatusLine().getStatusCode();
 	}
 
 	@Test
-	public void testVerifyFormBasedLogin() throws ClientProtocolException, IOException, URISyntaxException {
+	public void testCreateAUser() throws URISyntaxException, ParseException, IOException, JSONException {
 
-		HttpClient httpClient = new DefaultHttpClient();
+		HttpClient httpClient = createHttpClient();
 		try {
-			assertEquals(HttpStatus.SC_OK, login(httpClient));
+			HttpPost httpPost = new HttpPost(getOrionServerURI("/users"));
+			httpPost.setHeader(ProtocolConstants.HEADER_ORION_VERSION, "1");
+			List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+			nvps.add(new BasicNameValuePair("login", getOrionTestName()));
+			nvps.add(new BasicNameValuePair("password", getOrionTestName()));
+			nvps.add(new BasicNameValuePair("email", getOrionTestName() + "@test.com"));
+			httpPost.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"));
+			HttpResponse httpResponse = httpClient.execute(httpPost);
+			HttpEntity httpEntity = httpResponse.getEntity();
+			if (httpEntity != null) {
+				String result = EntityUtils.toString(httpEntity);
+				JSONObject jsonObject = new JSONObject(result);
+				String location = jsonObject.getString("Location");
+				String name = jsonObject.getString("Name");
+				System.out.println("Created User: " + name + " at Location: " + location);
+			}
+			assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode());
 		} finally {
 			httpClient.getConnectionManager().shutdown();
 		}
 	}
 
 	@Test
-	public void testVerifyWorkspace() throws ClientProtocolException, IOException, URISyntaxException {
+	public void testCreateAWorkspace() throws URISyntaxException, ParseException, IOException, JSONException {
 
-		HttpClient httpClient = new DefaultHttpClient();
+		HttpClient httpClient = createHttpClient();
 		try {
 			assertEquals(HttpStatus.SC_OK, login(httpClient));
 
-			HttpGet httpGet = new HttpGet(getOrionServerURI("/workspace"));
-			httpGet.setHeader(ProtocolConstants.HEADER_ORION_VERSION, "1");
-			HttpResponse httpResponse = httpClient.execute(httpGet);
-			System.out.println("Workspace Get: " + httpResponse.getStatusLine());
+			HttpPost httpPost = new HttpPost(getOrionServerURI("/workspace"));
+			httpPost.setHeader(ProtocolConstants.HEADER_ORION_VERSION, "1");
+			httpPost.setHeader(ProtocolConstants.HEADER_SLUG, "Orion Content");
+			HttpResponse httpResponse = httpClient.execute(httpPost);
 			HttpEntity httpEntity = httpResponse.getEntity();
 			if (httpEntity != null) {
-				String done = EntityUtils.toString(httpEntity);
-				System.out.println("- " + done);
+				String result = EntityUtils.toString(httpEntity);
+				JSONObject jsonObject = new JSONObject(result);
+				String location = jsonObject.getString("Location");
+				String name = jsonObject.getString("Name");
+				System.out.println("Created Workspace: " + name + " at Location: " + location);
+			}
+			assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode());
+		} finally {
+			httpClient.getConnectionManager().shutdown();
+		}
+	}
+
+	@Test
+	public void testCreateProject() throws ClientProtocolException, IOException, JSONException, URISyntaxException {
+
+		HttpClient httpClient = createHttpClient();
+		try {
+			assertEquals(HttpStatus.SC_OK, login(httpClient));
+
+			HttpPost httpPost = new HttpPost(getOrionServerURI("/workspace/" + getOrionTestName()));
+			httpPost.setHeader(ProtocolConstants.HEADER_ORION_VERSION, "1");
+			httpPost.setHeader(HTTP.CONTENT_TYPE, "application/json");
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("Name", getOrionTestName());
+			StringEntity stringEntity = new StringEntity(jsonObject.toString());
+			stringEntity.setContentType("application/json");
+			httpPost.setEntity(stringEntity);
+			HttpResponse httpResponse = httpClient.execute(httpPost);
+			HttpEntity httpEntity = httpResponse.getEntity();
+			if (httpEntity != null) {
+				String result = EntityUtils.toString(httpEntity);
+				jsonObject = new JSONObject(result);
+				String location = jsonObject.getString("Location");
+				String name = jsonObject.getString("Name");
+				System.out.println("Created Project: " + name + " at Location: " + location);
+			}
+			assertEquals(HttpStatus.SC_CREATED, httpResponse.getStatusLine().getStatusCode());
+		} finally {
+			httpClient.getConnectionManager().shutdown();
+		}
+	}
+
+	@Test
+	public void testGetProjects() throws ClientProtocolException, IOException, URISyntaxException, JSONException {
+
+		HttpClient httpClient = createHttpClient();
+		try {
+			assertEquals(HttpStatus.SC_OK, login(httpClient));
+
+			HttpGet httpGet = new HttpGet(getOrionServerURI("/workspace/" + getOrionTestName()));
+			httpGet.setHeader(ProtocolConstants.HEADER_ORION_VERSION, "1");
+			HttpResponse httpResponse = httpClient.execute(httpGet);
+			HttpEntity httpEntity = httpResponse.getEntity();
+			if (httpEntity != null) {
+				String result = EntityUtils.toString(httpEntity);
+				JSONObject jsonObject = new JSONObject(result);
+				JSONArray projects = jsonObject.getJSONArray("Projects");
+				String name = jsonObject.getString("Name");
+				if (projects.length() == 0) {
+					System.out.println("Found zero Projects in workspace named: " + name);
+				} else {
+					System.out.print("Found Projects in workspace named: " + name + " at locations: [ ");
+					for (int i = 0; i < projects.length(); i++) {
+						JSONObject project = projects.getJSONObject(i);
+						System.out.print(project.getString("Location") + " ");
+					}
+					System.out.println("]");
+				}
 			}
 			assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode());
 
@@ -108,28 +198,45 @@ public class MetaStoreTests {
 	}
 
 	@Test
-	public void testCreateProject() throws ClientProtocolException, IOException, JSONException, URISyntaxException {
+	public void testGetWorkspaces() throws ClientProtocolException, IOException, URISyntaxException, JSONException {
 
-		HttpClient httpClient = new DefaultHttpClient();
+		HttpClient httpClient = createHttpClient();
 		try {
 			assertEquals(HttpStatus.SC_OK, login(httpClient));
 
-			HttpPost httpPost = new HttpPost(getOrionServerURI("/workspace/test"));
-			httpPost.setHeader(ProtocolConstants.HEADER_ORION_VERSION, "1");
-			httpPost.setHeader(HTTP.CONTENT_TYPE, "application/json");
-			JSONObject jsonObject = new JSONObject();
-			jsonObject.put("Name", "project12");
-			StringEntity stringEntity = new StringEntity(jsonObject.toString());
-			stringEntity.setContentType("application/json");
-			httpPost.setEntity(stringEntity);
-			HttpResponse httpResponse = httpClient.execute(httpPost);
-			System.out.println("Create Project Post: " + httpResponse.getStatusLine());
+			HttpGet httpGet = new HttpGet(getOrionServerURI("/workspace"));
+			httpGet.setHeader(ProtocolConstants.HEADER_ORION_VERSION, "1");
+			HttpResponse httpResponse = httpClient.execute(httpGet);
 			HttpEntity httpEntity = httpResponse.getEntity();
 			if (httpEntity != null) {
-				String done = EntityUtils.toString(httpEntity);
-				System.out.println("- " + done);
+				String result = EntityUtils.toString(httpEntity);
+				JSONObject jsonObject = new JSONObject(result);
+				JSONArray workspaces = jsonObject.getJSONArray("Workspaces");
+				String name = jsonObject.getString("Name");
+				if (workspaces.length() == 0) {
+					System.out.println("Found zero Workspaces for user: " + name);
+				} else {
+					System.out.print("Found Workspaces for user: " + name + " at locations: [ ");
+					for (int i = 0; i < workspaces.length(); i++) {
+						JSONObject workspace = workspaces.getJSONObject(i);
+						System.out.print(workspace.getString("Location") + " ");
+					}
+					System.out.println("]");
+				}
 			}
-			assertEquals(HttpStatus.SC_CREATED, httpResponse.getStatusLine().getStatusCode());
+			assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode());
+
+		} finally {
+			httpClient.getConnectionManager().shutdown();
+		}
+	}
+
+	@Test
+	public void testVerifyFormBasedLogin() throws ClientProtocolException, IOException, URISyntaxException {
+
+		HttpClient httpClient = createHttpClient();
+		try {
+			assertEquals(HttpStatus.SC_OK, login(httpClient));
 		} finally {
 			httpClient.getConnectionManager().shutdown();
 		}
