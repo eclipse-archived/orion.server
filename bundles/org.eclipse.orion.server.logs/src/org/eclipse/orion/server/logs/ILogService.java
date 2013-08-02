60526a0020deff0cc557cf034451493e674df264
Mark Macdonald <Mark_MacDonald@ca.ibm.com>
2013-08-02 12:27:37 -0400

    [Debugging build breakage] Revert "Bug 413162 - Enable RollingFileAppender history access"
    This reverts commit ef89f35788b49d345a32de76da5821268a5d9eea.

--- bundles/org.eclipse.orion.server.logs/src/org/eclipse/orion/server/logs/ILogService.java
+++ bundles/org.eclipse.orion.server.logs/src/org/eclipse/orion/server/logs/ILogService.java
@@ -11,12 +11,10 @@
 
 package org.eclipse.orion.server.logs;
 
-import java.io.File;
 import java.util.List;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.FileAppender;
-import ch.qos.logback.core.rolling.RollingFileAppender;
 
 /**
  * Provides the OSGI Log service definition interface. The service handles
@@ -37,34 +35,4 @@ public interface ILogService {
 	 *         the current logger context.
 	 */
 	public FileAppender<ILoggingEvent> getFileAppender(String name);
-
-	/**
-	 * @param name
-	 *            Logback RollingFileAppender name property.
-	 * @return Appropriate RollingFileAppender or <code>null</code> if not
-	 *         present in the current logger context.
-	 */
-	public RollingFileAppender<ILoggingEvent> getRollingFileAppender(String name);
-
-	/**
-	 * @param rollingFileAppender
-	 *            The rolling file appender which log files are wished to be
-	 *            retrieved.
-	 * @return Appropriate File[] or <code>null</code> if none present in the
-	 *         current logger context. Note <code>null</code> might be returned
-	 *         also in case if the rollingFileAppender has no rolling policy
-	 *         defined.
-	 */
-	public File[] getArchivedLogFiles(RollingFileAppender<ILoggingEvent> rollingFileAppender);
-
-	/**
-	 * @param rollingFileAppender
-	 *            The rolling file appender which is responsible for populating
-	 *            the searched log file.
-	 * @param logFileName
-	 *            The required log file name.
-	 * @return Appropriate File or <code>null</code> if not present in the
-	 *         current logger context.
-	 */
-	public File getArchivedLogFile(RollingFileAppender<ILoggingEvent> rollingFileAppender, String logFileName);
 }
