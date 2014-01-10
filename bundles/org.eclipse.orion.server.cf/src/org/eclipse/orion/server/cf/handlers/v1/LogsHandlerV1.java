93da9c2cec5143f831730a377f74761fac50df1e
Malgorzata Janczarska <malgorzata.tomczyk@pl.ibm.com>
2014-01-10 18:50:17 +0100

    Bug 425254 - CF Logs API need to be enhanced

--- bundles/org.eclipse.orion.server.cf/src/org/eclipse/orion/server/cf/handlers/v1/LogsHandlerV1.java
+++ bundles/org.eclipse.orion.server.cf/src/org/eclipse/orion/server/cf/handlers/v1/LogsHandlerV1.java
@@ -10,18 +10,23 @@
  *******************************************************************************/
 package org.eclipse.orion.server.cf.handlers.v1;
 
+import java.net.URL;
+import java.net.URLDecoder;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.eclipse.core.runtime.*;
+import org.eclipse.orion.internal.server.core.IOUtilities;
 import org.eclipse.orion.internal.server.servlets.ServletResourceHandler;
 import org.eclipse.orion.server.cf.CFActivator;
-import org.eclipse.orion.server.cf.commands.GetLogsCommand;
+import org.eclipse.orion.server.cf.CFProtocolConstants;
+import org.eclipse.orion.server.cf.commands.*;
 import org.eclipse.orion.server.cf.jobs.CFJob;
 import org.eclipse.orion.server.cf.objects.Log;
 import org.eclipse.orion.server.cf.objects.Target;
 import org.eclipse.orion.server.cf.servlets.AbstractRESTHandler;
 import org.eclipse.orion.server.core.ServerStatus;
 import org.eclipse.osgi.util.NLS;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -46,15 +51,35 @@ public class LogsHandlerV1 extends AbstractRESTHandler<Log> {
 
 	@Override
 	protected CFJob handleGet(final Log resource, HttpServletRequest request, HttpServletResponse response, final String path) {
-
+		final String targetStr = IOUtilities.getQueryParameter(request, CFProtocolConstants.KEY_TARGET);
 		return new CFJob(request, false) {
 			@Override
 			protected IStatus performJob() {
 				try {
-					Target target = CFActivator.getDefault().getTargetRegistry().getTarget(userId);
+
+					Target target = null;
+					if (targetStr != null) {
+						JSONObject targetJSON = new JSONObject(URLDecoder.decode(targetStr, "UTF8"));
+						URL targetUrl = new URL(targetJSON.getString(CFProtocolConstants.KEY_URL));
+
+						target = CFActivator.getDefault().getTargetRegistry().getTarget(userId, targetUrl);
+						if (target == null) {
+							return new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_NOT_FOUND, "Target not set", null);
+						}
+
+						IStatus result = new SetOrgCommand(target, targetJSON.optString("Org")).doIt();
+						if (!result.isOK())
+							return result;
+
+						result = new SetSpaceCommand(target, targetJSON.optString("Space")).doIt();
+						if (!result.isOK())
+							return result;
+					} else {
+						target = CFActivator.getDefault().getTargetRegistry().getTarget(userId);
+					}
+
 					if (target == null) {
-						String msg = "Target not set"; //$NON-NLS-1$
-						return new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_NOT_FOUND, msg, null);
+						return new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_NOT_FOUND, "Target not set", null);
 					}
 
 					if (target.getSpace() == null) {
