8460ac7ca78ca1ac3e2748250c4c610e2fda18fb
Anthony Hunter <anthonyh@ca.ibm.com>
2014-04-23 18:09:11 -0400

    Bug 433238 Remove if else IMetaStore check from WorkspaceServiceTest

--- tests/org.eclipse.orion.server.tests/src/org/eclipse/orion/server/tests/servlets/workspace/WorkspaceServiceTest.java
+++ tests/org.eclipse.orion.server.tests/src/org/eclipse/orion/server/tests/servlets/workspace/WorkspaceServiceTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2013 IBM Corporation and others.
+ * Copyright (c) 2010, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,15 +15,23 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import com.meterware.httpunit.*;
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.filesystem.IFileStore;
-import org.eclipse.core.runtime.*;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.orion.internal.server.core.IOUtilities;
-import org.eclipse.orion.internal.server.core.metastore.SimpleMetaStore;
 import org.eclipse.orion.internal.server.servlets.ProtocolConstants;
 import org.eclipse.orion.internal.server.servlets.workspace.ServletTestingSupport;
 import org.eclipse.orion.internal.server.servlets.workspace.WorkspaceServlet;
@@ -32,11 +40,21 @@ import org.eclipse.orion.server.core.OrionConfiguration;
 import org.eclipse.orion.server.core.metastore.IMetaStore;
 import org.eclipse.orion.server.tests.servlets.files.FileSystemTest;
 import org.eclipse.orion.server.tests.servlets.internal.DeleteMethodWebRequest;
-import org.json.*;
-import org.junit.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.xml.sax.SAXException;
+
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.PostMethodWebRequest;
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
 
 /**
  * Tests for {@link WorkspaceServlet}.
@@ -114,6 +132,10 @@ public class WorkspaceServiceTest extends FileSystemTest {
 		assertNotNull(projectId);
 
 		//ensure project location = <workspace location>/project/<projectName>
+		IPath fullPath = new Path(locationHeader);
+		assertEquals(4, fullPath.segmentCount());
+		assertEquals("workspace", fullPath.segment(0));
+		String workspaceId = fullPath.segment(1);
 		URI projectLocation = new URI(toAbsoluteURI(locationHeader));
 		URI relative = workspaceLocation.relativize(projectLocation);
 		IPath projectPath = new Path(relative.getPath());
@@ -144,13 +166,9 @@ public class WorkspaceServiceTest extends FileSystemTest {
 		assertNotNull(contentLocation);
 
 		//ensure project content exists
-		if (OrionConfiguration.getMetaStore() instanceof SimpleMetaStore) {
-			// simple metastore, projects not in the same simple location anymore, so do not check
-		} else {
-			// legacy metastore, use what was in the original test
-			IFileStore projectStore = EFS.getStore(makeLocalPathAbsolute(projectId));
-			assertTrue(projectStore.fetchInfo().exists());
-		}
+		testProjectBaseLocation = "/" + workspaceId + '/' + projectName;
+		IFileStore projectStore = EFS.getStore(makeLocalPathAbsolute(""));
+		assertTrue(projectStore.fetchInfo().exists());
 
 		//add a file in the project
 		String fileName = "file.txt";
@@ -589,6 +607,7 @@ public class WorkspaceServiceTest extends FileSystemTest {
 		JSONObject project = new JSONObject(response.getText());
 		testProjectLocalFileLocation = "/" + project.optString(ProtocolConstants.KEY_ID, null);
 		String contentLocation = project.optString(ProtocolConstants.KEY_CONTENT_LOCATION);
+		IFileStore projectStore = EFS.getStore(makeLocalPathAbsolute(""));
 
 		//add a file in the project
 		String fileName = "file.txt";
@@ -617,16 +636,7 @@ public class WorkspaceServiceTest extends FileSystemTest {
 		assertEquals(0, projects.length());
 
 		//ensure project content is deleted
-		if (OrionConfiguration.getMetaStore() instanceof SimpleMetaStore) {
-			// simple metastore, projects not in the same simple location anymore, so do not check
-			IFileStore projectStore = EFS.getStore(makeLocalPathAbsolute(""));
-			assertFalse(projectStore.fetchInfo().exists());
-		} else {
-			// legacy metastore, use what was in the original test
-			String projectId = project.getString(ProtocolConstants.KEY_ID);
-			IFileStore projectStore = EFS.getStore(makeLocalPathAbsolute(projectId));
-			assertFalse(projectStore.fetchInfo().exists());
-		}
+		assertFalse(projectStore.fetchInfo().exists());
 
 		//deleting again should be safe (DELETE is idempotent)
 		response = webConversation.getResponse(request);
