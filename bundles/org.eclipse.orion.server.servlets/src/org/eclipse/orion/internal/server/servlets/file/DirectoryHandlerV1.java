d1c7d115d5b32cab62fb920fd96dfae3ca1dfd26
Bogdan Gheorghe <gheorghe@ca.ibm.com>
2014-07-03 14:04:36 -0400

    DBCS debug

--- bundles/org.eclipse.orion.server.servlets/src/org/eclipse/orion/internal/server/servlets/file/DirectoryHandlerV1.java
+++ bundles/org.eclipse.orion.server.servlets/src/org/eclipse/orion/internal/server/servlets/file/DirectoryHandlerV1.java
@@ -103,6 +103,7 @@ public class DirectoryHandlerV1 extends ServletResourceHandler<IFileStore> {
 			//write the response
 			URI location = URIUtil.append(getURI(request), name);
 			JSONObject result = ServletFileStoreHandler.toJSON(toCreate, toCreate.fetchInfo(EFS.NONE, null), location);
+			result.append("FileEncoding", System.getProperty("file.encoding"));
 			OrionServlet.writeJSONResponse(request, response, result);
 			response.setHeader(ProtocolConstants.HEADER_LOCATION, ServletResourceHandler.resovleOrionURI(request, location).toString());
 			//response code should indicate if a new resource was actually created or not
