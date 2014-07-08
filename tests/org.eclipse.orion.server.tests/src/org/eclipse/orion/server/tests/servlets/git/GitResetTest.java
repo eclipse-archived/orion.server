b390e322f5377a7f0c38395361cea10548b7c365
Anthony Hunter <anthonyh@ca.ibm.com>
2014-07-08 11:56:32 -0400

    Bug 439147 [tests] GitResetTest.testResetNotImplemented() is failing

--- tests/org.eclipse.orion.server.tests/src/org/eclipse/orion/server/tests/servlets/git/GitResetTest.java
+++ tests/org.eclipse.orion.server.tests/src/org/eclipse/orion/server/tests/servlets/git/GitResetTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 IBM Corporation and others
+ * Copyright (c) 2011, 2014 IBM Corporation and others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -133,10 +133,6 @@ public class GitResetTest extends GitTest {
 		assertEquals(HttpURLConnection.HTTP_NOT_IMPLEMENTED, response.getResponseCode());
 
 		request = getPostGitIndexRequest(gitIndexUri, ResetType.MERGE);
-		response = webConversation.getResponse(request);
-		assertEquals(HttpURLConnection.HTTP_NOT_IMPLEMENTED, response.getResponseCode());
-
-		request = getPostGitIndexRequest(gitIndexUri, ResetType.SOFT);
 		response = webConversation.getResponse(request);
 		assertEquals(HttpURLConnection.HTTP_NOT_IMPLEMENTED, response.getResponseCode());
 	}
