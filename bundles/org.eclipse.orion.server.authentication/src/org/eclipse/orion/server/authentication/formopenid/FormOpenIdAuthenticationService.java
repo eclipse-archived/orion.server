790c05570b2f1ae0e0aed89f23fcd2a5c7b667b2
John Arthorne <John_Arthorne@ca.ibm.com>
2014-07-22 15:11:20 -0400

    Fixed compiler warnings

--- bundles/org.eclipse.orion.server.authentication/src/org/eclipse/orion/server/authentication/formopenid/FormOpenIdAuthenticationService.java
+++ bundles/org.eclipse.orion.server.authentication/src/org/eclipse/orion/server/authentication/formopenid/FormOpenIdAuthenticationService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2012 IBM Corporation and others 
+ * Copyright (c) 2010, 2014 IBM Corporation and others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,11 +11,8 @@
 package org.eclipse.orion.server.authentication.formopenid;
 
 import java.io.IOException;
-import java.util.Properties;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
-
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.orion.internal.server.servlets.ProtocolConstants;
