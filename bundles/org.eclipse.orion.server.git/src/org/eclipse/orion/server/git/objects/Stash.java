19670761046f3f87e9e77cb147c9b3cac4e8d1ca
Bartosz Grabski <bartosz.grabski@pl.ibm.com>
2014-07-23 15:41:44 +0200

    Bug 359273 - support git stash - WIP server side
    Change-Id: I81669dec40f33cf378b8e611544b19aa7ea2f9e2
    Signed-off-by: Bartosz Grabski <bartosz.grabski@pl.ibm.com>

---
+++ bundles/org.eclipse.orion.server.git/src/org/eclipse/orion/server/git/objects/Stash.java
@@ -0,0 +1,32 @@
+package org.eclipse.orion.server.git.objects;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Comparator;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.orion.server.core.resources.annotations.ResourceDescription;
+
+@ResourceDescription(type = Stash.TYPE)
+public class Stash extends GitObject {
+
+	public static final String RESOURCE = "stash";
+	public static final String TYPE = "Stash";
+	public static final Comparator<RevCommit> COMPARATOR = new Comparator<RevCommit>() {
+
+		@Override
+		public int compare(RevCommit o1, RevCommit o2) {
+			return o1.getCommitTime() > o2.getCommitTime() ? 1 : o1.getId().equals(o2.getId()) ? 0 : -1;
+		}
+	};
+
+	Stash(URI cloneLocation, Repository db) {
+		super(cloneLocation, db);
+	}
+
+	@Override
+	protected URI getLocation() throws URISyntaxException {
+		return null;
+	}
+
+}
