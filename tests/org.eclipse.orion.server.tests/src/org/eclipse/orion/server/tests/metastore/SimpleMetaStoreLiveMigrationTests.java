9f2311ce10e57f69d48d7964db331f3e24675ea1
Anthony Hunter <anthonyh@ca.ibm.com>
2014-07-30 16:38:28 -0400

    Bug 439732 one metadata version: test user.json migration (in progress)

--- tests/org.eclipse.orion.server.tests/src/org/eclipse/orion/server/tests/metastore/SimpleMetaStoreLiveMigrationTests.java
+++ tests/org.eclipse.orion.server.tests/src/org/eclipse/orion/server/tests/metastore/SimpleMetaStoreLiveMigrationTests.java
@@ -26,7 +26,6 @@ import org.eclipse.orion.internal.server.core.metastore.SimpleMetaStoreUtil;
 import org.eclipse.orion.internal.server.core.metastore.SimpleUserPasswordUtil;
 import org.eclipse.orion.internal.server.servlets.ProtocolConstants;
 import org.eclipse.orion.server.tests.servlets.files.FileSystemTest;
-import org.eclipse.orion.server.useradmin.User;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Before;
@@ -45,6 +44,16 @@ import com.meterware.httpunit.WebResponse;
  * @author Anthony Hunter
  */
 public class SimpleMetaStoreLiveMigrationTests extends FileSystemTest {
+
+	/**
+	 * The first version of the Simple Meta Store was version 4.
+	 */
+	public final static int VERSION4 = 4;
+
+	/**
+	 * The second version of the Simple Meta Store was version 6.
+	 */
+	public final static int VERSION6 = 6;
 
 	@BeforeClass
 	public static void initializeRootFileStorePrefixLocation() {
@@ -111,17 +120,6 @@ public class SimpleMetaStoreLiveMigrationTests extends FileSystemTest {
 		return jsonObject;
 	}
 
-	/**
-	 * A user with no workspaces created by the tests framework.
-	 * @throws Exception
-	 */
-	@Test
-	public void testUserWithNoWorkspaces() throws Exception {
-		User testUser = createUser(testUserLogin, testUserPassword);
-		testUserId = testUser.getUid();
-		verifyWorkspaceRequest(0);
-	}
-
 	protected void createUserMetaData(JSONObject newUserJSON) throws CoreException {
 		SimpleMetaStoreUtil.createMetaUserFolder(getWorkspaceRoot(), testUserId);
 		File userMetaFolder = SimpleMetaStoreUtil.readMetaUserFolder(getWorkspaceRoot(), testUserId);
@@ -130,42 +128,6 @@ public class SimpleMetaStoreLiveMigrationTests extends FileSystemTest {
 		SimpleMetaStoreUtil.createMetaFile(userMetaFolder, SimpleMetaStore.USER, newUserJSON);
 		File userMetaFile = SimpleMetaStoreUtil.retrieveMetaFile(userMetaFolder, SimpleMetaStore.USER);
 		assertTrue("Could not create file " + userMetaFile.toString(), userMetaFile.exists() && userMetaFile.isFile());
-	}
-
-	/**
-	 * A user with no workspaces in SimpleMetaStore version 4 format.
-	 * @throws Exception
-	 */
-	@Test
-	public void testUserWithNoWorkspacesVersion4() throws Exception {
-		testUserId = testName.getMethodName();
-		JSONObject newUserJSON = createUserJson(4, testUserId, new ArrayList<String>());
-		createUserMetaData(newUserJSON);
-		verifyWorkspaceRequest(0);
-	}
-
-	/**
-	 * A user with no workspaces in SimpleMetaStore version 6 format.
-	 * @throws Exception
-	 */
-	@Test
-	public void testUserWithNoWorkspacesVersion6() throws Exception {
-		testUserId = testName.getMethodName();
-		JSONObject newUserJSON = createUserJson(6, testUserId, new ArrayList<String>());
-		createUserMetaData(newUserJSON);
-		verifyWorkspaceRequest(0);
-	}
-
-	/**
-	 * A user with no workspaces in SimpleMetaStore version 4 format.
-	 * @throws Exception
-	 */
-	@Test
-	public void testUserWithNoWorkspacesVersion7() throws Exception {
-		testUserId = testName.getMethodName();
-		JSONObject newUserJSON = createUserJson(SimpleMetaStore.VERSION, testUserId, new ArrayList<String>());
-		createUserMetaData(newUserJSON);
-		verifyWorkspaceRequest(0);
 	}
 
 	@Before
@@ -178,7 +140,7 @@ public class SimpleMetaStoreLiveMigrationTests extends FileSystemTest {
 	 * This test does not actually test anything, just verifies the framework is running as expected.
 	 * @throws Exception
 	 */
-	// @Test
+	@Test
 	public void testBasicFrameworkTest() throws Exception {
 		// perform the basic steps from the parent abstract test class.
 		setUpAuthorization();
@@ -190,6 +152,58 @@ public class SimpleMetaStoreLiveMigrationTests extends FileSystemTest {
 		String fileName = createSampleFile(directoryPath);
 
 		verifySampleFileContents(directoryPath, fileName);
+		verifyWorkspaceRequest(1);
+		verifyUserMetaData(testUserId);
+	}
+
+	/**
+	 * A user with no workspaces created by the tests framework.
+	 * @throws Exception
+	 */
+	@Test
+	public void testUserWithNoWorkspaces() throws Exception {
+		setUpAuthorization();
+		verifyWorkspaceRequest(0);
+		verifyUserMetaData(testUserId);
+	}
+
+	/**
+	 * A user with no workspaces in SimpleMetaStore version 4 format.
+	 * @throws Exception
+	 */
+	@Test
+	public void testUserWithNoWorkspacesVersion4() throws Exception {
+		testUserId = testName.getMethodName();
+		JSONObject newUserJSON = createUserJson(VERSION4, testUserId, new ArrayList<String>());
+		createUserMetaData(newUserJSON);
+		verifyWorkspaceRequest(0);
+		verifyUserMetaData(testUserId);
+	}
+
+	/**
+	 * A user with no workspaces in SimpleMetaStore version 6 format.
+	 * @throws Exception
+	 */
+	@Test
+	public void testUserWithNoWorkspacesVersion6() throws Exception {
+		testUserId = testName.getMethodName();
+		JSONObject newUserJSON = createUserJson(VERSION6, testUserId, new ArrayList<String>());
+		createUserMetaData(newUserJSON);
+		verifyWorkspaceRequest(0);
+		verifyUserMetaData(testUserId);
+	}
+
+	/**
+	 * A user with no workspaces in SimpleMetaStore version 7 format.
+	 * @throws Exception
+	 */
+	@Test
+	public void testUserWithNoWorkspacesVersion7() throws Exception {
+		testUserId = testName.getMethodName();
+		JSONObject newUserJSON = createUserJson(SimpleMetaStore.VERSION, testUserId, new ArrayList<String>());
+		createUserMetaData(newUserJSON);
+		verifyWorkspaceRequest(0);
+		verifyUserMetaData(testUserId);
 	}
 
 	/**
@@ -204,6 +218,30 @@ public class SimpleMetaStoreLiveMigrationTests extends FileSystemTest {
 		WebResponse response = webConversation.getResponse(request);
 		assertEquals(HttpURLConnection.HTTP_OK, response.getResponseCode());
 		assertEquals("Invalid file content", fileName, response.getText());
+	}
+
+	protected void verifyUserJson(JSONObject jsonObject, String userId) throws Exception {
+		assertTrue(jsonObject.has(SimpleMetaStore.ORION_VERSION));
+		assertEquals("OrionVersion is incorrect", SimpleMetaStore.VERSION, jsonObject.getInt(SimpleMetaStore.ORION_VERSION));
+		assertTrue(jsonObject.has("UniqueId"));
+		assertEquals(userId, jsonObject.getString("UniqueId"));
+		assertTrue(jsonObject.has("UserName"));
+		assertEquals(userId, jsonObject.getString("UserName"));
+		assertTrue(jsonObject.has("FullName"));
+		assertTrue(jsonObject.has("WorkspaceIds"));
+		assertTrue(jsonObject.has("password"));
+		assertTrue(jsonObject.has("Properties"));
+		JSONObject properties = jsonObject.getJSONObject("Properties");
+		assertTrue(properties.has("UserRightsVersion"));
+		assertTrue(properties.has("UserRights"));
+	}
+
+	protected void verifyUserMetaData(String testUserId) throws Exception {
+		assertTrue(SimpleMetaStoreUtil.isMetaUserFolder(getWorkspaceRoot(), testUserId));
+		File userMetaFolder = SimpleMetaStoreUtil.readMetaUserFolder(getWorkspaceRoot(), testUserId);
+		assertTrue(SimpleMetaStoreUtil.isMetaFile(userMetaFolder, SimpleMetaStore.USER));
+		JSONObject userJSON = SimpleMetaStoreUtil.readMetaFile(userMetaFolder, SimpleMetaStore.USER);
+		verifyUserJson(userJSON, testUserId);
 	}
 
 	/**
