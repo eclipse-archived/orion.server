78d5bbac296ffa5b000055b4d8caadeabf02a74c
Anthony Hunter <anthonyh@ca.ibm.com>
2014-08-07 16:55:15 -0400

    Bug 439732 one metadata version: Added SimpleMetaStoreLiveMigrationTests

--- tests/org.eclipse.orion.server.tests/src/org/eclipse/orion/server/tests/AllServerTests.java
+++ tests/org.eclipse.orion.server.tests/src/org/eclipse/orion/server/tests/AllServerTests.java
@@ -13,6 +13,7 @@ package org.eclipse.orion.server.tests;
 import org.eclipse.orion.server.tests.cf.AllCFTests;
 import org.eclipse.orion.server.tests.metastore.ProjectInfoTests;
 import org.eclipse.orion.server.tests.metastore.SimpleMetaStoreConcurrencyTests;
+import org.eclipse.orion.server.tests.metastore.SimpleMetaStoreLiveMigrationTests;
 import org.eclipse.orion.server.tests.metastore.SimpleMetaStoreTests;
 import org.eclipse.orion.server.tests.metastore.SimpleMetaStoreUtilTest;
 import org.eclipse.orion.server.tests.metastore.SimpleUserPasswordUtilTests;
@@ -38,7 +39,7 @@ import org.junit.runners.Suite.SuiteClasses;
  * Runs all automated server tests.
  */
 @RunWith(Suite.class)
-@SuiteClasses({SearchTest.class, TransferTest.class, Base64Test.class, CoreFilesTest.class, AdvancedFilesTest.class, PreferenceTest.class, BasicUsersTest.class, WorkspaceServiceTest.class, AllSiteTests.class, AllGitTests.class, AllTaskTests.class, OrionCredentialsServiceTest.class, OrionUserProfileServiceTest.class, MetaStoreTest.class, ProjectInfoTests.class, SimpleMetaStoreTests.class, SimpleMetaStoreUtilTest.class, UserInfoTests.class, WorkspaceInfoTests.class, SimpleUserPasswordUtilTests.class, SimpleMetaStoreConcurrencyTests.class, AllCFTests.class})
+@SuiteClasses({SearchTest.class, TransferTest.class, Base64Test.class, CoreFilesTest.class, AdvancedFilesTest.class, PreferenceTest.class, BasicUsersTest.class, WorkspaceServiceTest.class, AllSiteTests.class, AllGitTests.class, AllTaskTests.class, OrionCredentialsServiceTest.class, OrionUserProfileServiceTest.class, MetaStoreTest.class, ProjectInfoTests.class, SimpleMetaStoreTests.class, SimpleMetaStoreLiveMigrationTests.class, SimpleMetaStoreUtilTest.class, UserInfoTests.class, WorkspaceInfoTests.class, SimpleUserPasswordUtilTests.class, SimpleMetaStoreConcurrencyTests.class, AllCFTests.class})
 public class AllServerTests {
 	//goofy junit4, no class body needed
 }
