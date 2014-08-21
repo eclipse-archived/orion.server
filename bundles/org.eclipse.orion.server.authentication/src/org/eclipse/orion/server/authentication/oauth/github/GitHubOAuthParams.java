00e780676cfb9fd034a74aa8b4466886c0967b38
Aidan Redpath <aidanr@ca.ibm.com>
2014-08-21 16:31:29 -0400

    Access oauth keys through web-ide.conf
    Change-Id: I0ca734c9de7057128050112b7d9acf6776eee466
    Signed-off-by: Aidan Redpath <aidanr@ca.ibm.com>

--- bundles/org.eclipse.orion.server.authentication/src/org/eclipse/orion/server/authentication/oauth/github/GitHubOAuthParams.java
+++ bundles/org.eclipse.orion.server.authentication/src/org/eclipse/orion/server/authentication/oauth/github/GitHubOAuthParams.java
@@ -19,6 +19,7 @@ import org.eclipse.orion.server.authentication.oauth.OAuthConsumer;
 import org.eclipse.orion.server.authentication.oauth.OAuthException;
 import org.eclipse.orion.server.authentication.oauth.OAuthParams;
 import org.eclipse.orion.server.authentication.oauth.OAuthTokenResponse;
+import org.eclipse.orion.server.core.PreferenceHelper;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -29,45 +30,49 @@ import org.json.JSONObject;
  *
  */
 public class GitHubOAuthParams extends OAuthParams {
-	
+
 	private static final OAuthProviderType PROVIDER_TYPE = OAuthProviderType.GITHUB;
-	
+
 	private static final String GITHUB = "GitHub";
-		
+
 	private static final String REDIRECT_URI_LOGIN = "/login/oauth";
-	
+
 	private static final String REDIRECT_URI_LINK = "/mixlogin/manageoauth/oauth";
-	
+
 	private static final String RESPONSE_TYPE = "code";
-	
+
 	private static final String SCOPE = "user";
-	
+
+	private static final String CLIENT_KEY = "orion.oauth.github.client";
+
+	private static final String CLIENT_SECRET = "orion.oauth.github.secret";
+
 	private static final GrantType GRANT_TYPE = GrantType.AUTHORIZATION_CODE;
-	
+
 	private static final Class<? extends OAuthAccessTokenResponse> TOKEN_RESPONSE_CLASS = OAuthTokenResponse.class;
-	
+
 	private String client_key = null;
 	private String client_secret = null;
-	
+
 
 	public GitHubOAuthParams(HttpServletRequest req, boolean login) throws OAuthException {
 		super(req, login);
 	}
-	
+
 	public OAuthProviderType getProviderType() {
 		return PROVIDER_TYPE;
 	}
 
 	public String getClientKey() throws OAuthException {
 		if(client_key == null) {
-			setCredentials();
+			client_key = PreferenceHelper.getString(CLIENT_KEY);
 		}
 		return client_key;
 	}
 
 	public String getClientSecret() throws OAuthException {
 		if(client_secret == null) {
-			setCredentials();
+			client_secret = PreferenceHelper.getString(CLIENT_SECRET);
 		}
 		return client_secret;
 	}
@@ -91,22 +96,8 @@ public class GitHubOAuthParams extends OAuthParams {
 	public Class<? extends OAuthAccessTokenResponse> getTokenResponseClass() {
 		return TOKEN_RESPONSE_CLASS;
 	}
-	
+
 	public OAuthConsumer getNewOAuthConsumer(OAuthAccessTokenResponse oauthAccessTokenResponse) throws OAuthException {
 		return new GitHubOAuthConsumer(oauthAccessTokenResponse);
 	}
-	
-	
-	private void setCredentials() throws OAuthException{
-		JSONObject json = readCredentialFile();
-		try { 
-			JSONObject googleCredentials = json.getJSONObject(GITHUB);
-			client_key = googleCredentials.getString(CLIENT_KEY);
-			client_secret = googleCredentials.getString(CLIENT_SECRET);
-		} catch (JSONException e) {
-			throw new OAuthException("Error getting oauth credentials");
-		}
-	}
-
-
 }
