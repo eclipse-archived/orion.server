852aa006f628ce9c26063aa580a79eefeba108e4
Anthony Hunter <anthonyh@ca.ibm.com>
2014-09-16 15:25:07 -0400

    Bug 444304 GitIndexHandlerV1 is masking GitAPIExceptions

--- bundles/org.eclipse.orion.server.git/src/org/eclipse/orion/server/git/servlets/GitIndexHandlerV1.java
+++ bundles/org.eclipse.orion.server.git/src/org/eclipse/orion/server/git/servlets/GitIndexHandlerV1.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2012 IBM Corporation and others.
+ * Copyright (c) 2011, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,31 +10,43 @@
  *******************************************************************************/
 package org.eclipse.orion.server.git.servlets;
 
-import org.eclipse.orion.server.core.ProtocolConstants;
-
-import org.eclipse.orion.internal.server.servlets.ServletResourceHandler;
-import org.eclipse.orion.server.servlets.OrionServlet;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map.Entry;
 import java.util.Set;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.eclipse.core.runtime.*;
-import org.eclipse.jgit.api.*;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jgit.api.AddCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.ResetCommand;
 import org.eclipse.jgit.api.ResetCommand.ResetType;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.NoFilepatternException;
-import org.eclipse.jgit.lib.*;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.ObjectStream;
+import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.orion.internal.server.servlets.ServletResourceHandler;
 import org.eclipse.orion.internal.server.servlets.workspace.authorization.AuthorizationService;
-import org.eclipse.orion.server.core.*;
+import org.eclipse.orion.server.core.IOUtilities;
+import org.eclipse.orion.server.core.LogHelper;
+import org.eclipse.orion.server.core.ProtocolConstants;
+import org.eclipse.orion.server.core.ServerStatus;
 import org.eclipse.orion.server.git.GitConstants;
 import org.eclipse.orion.server.git.objects.Index;
 import org.eclipse.orion.server.git.servlets.GitUtils.Traverse;
+import org.eclipse.orion.server.servlets.OrionServlet;
 import org.eclipse.osgi.util.NLS;
-import org.json.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * A handler for Git operations on index:
@@ -68,14 +80,14 @@ public class GitIndexHandlerV1 extends ServletResourceHandler<String> {
 				return false; // TODO: or an error response code, 405?
 			db = FileRepositoryBuilder.create(gitDir);
 			switch (getMethod(request)) {
-				case GET :
-					return handleGet(request, response, db, GitUtils.getRelativePath(p, set.iterator().next().getKey()));
-				case PUT :
-					return handlePut(request, response, db, GitUtils.getRelativePath(p, set.iterator().next().getKey()));
-				case POST :
-					return handlePost(request, response, db, p);
-				default :
-					//fall through and return false below
+			case GET:
+				return handleGet(request, response, db, GitUtils.getRelativePath(p, set.iterator().next().getKey()));
+			case PUT:
+				return handlePut(request, response, db, GitUtils.getRelativePath(p, set.iterator().next().getKey()));
+			case POST:
+				return handlePost(request, response, db, p);
+			default:
+				// fall through and return false below
 			}
 		} catch (Exception e) {
 			String msg = NLS.bind("Failed to process an operation on index for {0}", path); //$NON-NLS-1$
@@ -100,7 +112,7 @@ public class GitIndexHandlerV1 extends ServletResourceHandler<String> {
 		return true;
 	}
 
-	private boolean handlePut(HttpServletRequest request, HttpServletResponse response, Repository db, String pattern) throws GitAPIException, JSONException, IOException {
+	private boolean handlePut(HttpServletRequest request, HttpServletResponse response, Repository db, String pattern) throws ServletException, JSONException, IOException {
 		JSONObject toAdd = OrionServlet.readJSONRequest(request);
 		JSONArray paths = toAdd.optJSONArray(ProtocolConstants.KEY_PATH);
 		if (paths == null) {
@@ -113,7 +125,11 @@ public class GitIndexHandlerV1 extends ServletResourceHandler<String> {
 			add.addFilepattern(paths.getString(i));
 		}
 		// "git add {pattern}"
-		add.call();
+		try {
+			add.call();
+		} catch (GitAPIException e) {
+			return statusHandler.handleRequest(request, response, new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage(), e));
+		}
 
 		// TODO: we're calling "add" twice, this is inefficient, see bug 349299
 		// "git add -u {pattern}"
@@ -121,7 +137,11 @@ public class GitIndexHandlerV1 extends ServletResourceHandler<String> {
 		for (int i = 0; i < paths.length(); i++) {
 			add.addFilepattern(paths.getString(i));
 		}
-		add.call();
+		try {
+			add.call();
+		} catch (GitAPIException e) {
+			return statusHandler.handleRequest(request, response, new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage(), e));
+		}
 		return true;
 	}
 
@@ -131,28 +151,28 @@ public class GitIndexHandlerV1 extends ServletResourceHandler<String> {
 		if (resetType != null) {
 			JSONArray paths = toReset.optJSONArray(ProtocolConstants.KEY_PATH);
 			if (paths != null) {
-				String msg = NLS.bind("Mixing {0} and {1} parameters is not allowed.", new Object[] {ProtocolConstants.KEY_PATH, GitConstants.KEY_RESET_TYPE});
+				String msg = NLS.bind("Mixing {0} and {1} parameters is not allowed.", new Object[] { ProtocolConstants.KEY_PATH, GitConstants.KEY_RESET_TYPE });
 				return statusHandler.handleRequest(request, response, new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_BAD_REQUEST, msg, null));
 			}
 			String ref = toReset.optString(GitConstants.KEY_TAG_COMMIT, Constants.HEAD);
 			try {
 				ResetType type = ResetType.valueOf(resetType);
 				switch (type) {
-					case MIXED :
-					case HARD :
-					case SOFT :
-						Git git = new Git(db);
-						// "git reset --{type} HEAD ."
-						try {
-							git.reset().setMode(type).setRef(ref).call();
-						} catch (GitAPIException e) {
-							statusHandler.handleRequest(request, response, new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage(), e));
-						}
-						return true;
-					case KEEP :
-					case MERGE :
-						String msg = NLS.bind("The reset type is not yet supported: {0}.", resetType);
-						return statusHandler.handleRequest(request, response, new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_NOT_IMPLEMENTED, msg, null));
+				case MIXED:
+				case HARD:
+				case SOFT:
+					Git git = new Git(db);
+					// "git reset --{type} HEAD ."
+					try {
+						git.reset().setMode(type).setRef(ref).call();
+					} catch (GitAPIException e) {
+						statusHandler.handleRequest(request, response, new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage(), e));
+					}
+					return true;
+				case KEEP:
+				case MERGE:
+					String msg = NLS.bind("The reset type is not yet supported: {0}.", resetType);
+					return statusHandler.handleRequest(request, response, new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_NOT_IMPLEMENTED, msg, null));
 				}
 			} catch (IllegalArgumentException e) {
 				String msg = NLS.bind("Unknown or malformed reset type: {0}.", resetType);
@@ -161,7 +181,7 @@ public class GitIndexHandlerV1 extends ServletResourceHandler<String> {
 		} else {
 			String commit = toReset.optString(GitConstants.KEY_TAG_COMMIT, null);
 			if (commit != null) {
-				String msg = NLS.bind("Mixing {0} and {1} parameters is not allowed.", new Object[] {ProtocolConstants.KEY_PATH, GitConstants.KEY_TAG_COMMIT});
+				String msg = NLS.bind("Mixing {0} and {1} parameters is not allowed.", new Object[] { ProtocolConstants.KEY_PATH, GitConstants.KEY_TAG_COMMIT });
 				return statusHandler.handleRequest(request, response, new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_BAD_REQUEST, msg, null));
 			}
 			JSONArray paths = toReset.optJSONArray(ProtocolConstants.KEY_PATH);
@@ -172,7 +192,7 @@ public class GitIndexHandlerV1 extends ServletResourceHandler<String> {
 					reset.addPath(paths.getString(i));
 				}
 			} else {
-				//path format is /file/{workspaceId}/{projectName}[/{path}]
+				// path format is /file/{workspaceId}/{projectName}[/{path}]
 				String projectRelativePath = path.removeFirstSegments(3).toString();
 				if (projectRelativePath.isEmpty()) {
 					String msg = "Path cannot be empty.";
