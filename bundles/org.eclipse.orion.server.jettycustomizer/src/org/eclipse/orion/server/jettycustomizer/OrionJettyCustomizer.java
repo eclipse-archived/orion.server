5960e16ff7027d78fcf9e920318ccc9a88cfeee0
Anthony Hunter <anthonyh@ca.ibm.com>
2014-09-23 22:14:30 -0400

    Bug 443966 Cleanup Java source in org.eclipse.orion.server.jettycustomizer

--- bundles/org.eclipse.orion.server.jettycustomizer/src/org/eclipse/orion/server/jettycustomizer/OrionJettyCustomizer.java
+++ bundles/org.eclipse.orion.server.jettycustomizer/src/org/eclipse/orion/server/jettycustomizer/OrionJettyCustomizer.java
@@ -32,8 +32,7 @@ import org.slf4j.LoggerFactory;
 public final class OrionJettyCustomizer extends JettyCustomizer {
 
 	@Override
-	public Object customizeContext(Object context,
-			Dictionary<String, ?> settings) {
+	public Object customizeContext(Object context, Dictionary<String, ?> settings) {
 		if (context instanceof ServletContextHandler) {
 			ServletContextHandler jettyContext = (ServletContextHandler) context;
 
@@ -42,15 +41,13 @@ public final class OrionJettyCustomizer extends JettyCustomizer {
 			try {
 				rootLocation = fileStore.toLocalFile(EFS.NONE, null);
 			} catch (CoreException e) {
-				Logger logger = LoggerFactory
-						.getLogger("org.eclipse.orion.server.config"); //$NON-NLS-1$
+				Logger logger = LoggerFactory.getLogger("org.eclipse.orion.server.config"); //$NON-NLS-1$
 				logger.error("Could not initialize NCSA Request Log", e); //$NON-NLS-1$
 			}
 
 			File metadata = new File(rootLocation, ".metadata");
 			if (!metadata.exists() && !metadata.isDirectory()) {
-				Logger logger = LoggerFactory
-						.getLogger("org.eclipse.orion.server.config"); //$NON-NLS-1$
+				Logger logger = LoggerFactory.getLogger("org.eclipse.orion.server.config"); //$NON-NLS-1$
 				logger.error("Could not initialize NCSA Request Log: Folder does not exist: " + metadata.toString()); //$NON-NLS-1$
 			}
 
@@ -58,14 +55,12 @@ public final class OrionJettyCustomizer extends JettyCustomizer {
 			if (!logsFolder.exists()) {
 				logsFolder.mkdir();
 				if (!logsFolder.exists() && !logsFolder.isDirectory()) {
-					Logger logger = LoggerFactory
-							.getLogger("org.eclipse.orion.server.config"); //$NON-NLS-1$
+					Logger logger = LoggerFactory.getLogger("org.eclipse.orion.server.config"); //$NON-NLS-1$
 					logger.error("Could not initialize NCSA Request Log: Logs folder does not exist: " + logsFolder.toString()); //$NON-NLS-1$
 				}
 			}
 
-			NCSARequestLog requestLog = new NCSARequestLog(
-					logsFolder.toString() + File.separator + "orion-access-yyyy_mm_dd.log");
+			NCSARequestLog requestLog = new NCSARequestLog(logsFolder.toString() + File.separator + "orion-access-yyyy_mm_dd.log");
 			requestLog.setRetainDays(90);
 			requestLog.setAppend(true);
 			requestLog.setExtended(true);
@@ -75,8 +70,7 @@ public final class OrionJettyCustomizer extends JettyCustomizer {
 			requestLogHandler.setRequestLog(requestLog);
 
 			jettyContext.setHandler(requestLogHandler);
-			Logger logger = LoggerFactory
-					.getLogger("org.eclipse.orion.server.config"); //$NON-NLS-1$
+			Logger logger = LoggerFactory.getLogger("org.eclipse.orion.server.config"); //$NON-NLS-1$
 			logger.info("Initialized NCSA Request Logs in " + logsFolder.toString()); //$NON-NLS-1$
 		}
 		return super.customizeContext(context, settings);
