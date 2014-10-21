573dd235d2bca06193eedeb7216be7bc10d951fb
Anthony Hunter <anthonyh@ca.ibm.com>
2014-10-21 17:04:03 -0400

    Bug 444501 Remove IOrionCredentialsService and IOrionUserProfileService

--- bundles/org.eclipse.orion.server.authentication/src/org/eclipse/orion/server/authentication/formoauth/FormAuthLoginServlet.java
+++ bundles/org.eclipse.orion.server.authentication/src/org/eclipse/orion/server/authentication/formoauth/FormAuthLoginServlet.java
@@ -22,9 +22,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.eclipse.orion.server.authentication.form.FormAuthHelper;
 import org.eclipse.orion.server.authentication.form.FormAuthHelper.LoginResult;
 import org.eclipse.orion.server.authentication.oauth.OAuthException;
-import org.eclipse.orion.server.core.LogHelper;
 import org.eclipse.orion.server.core.resources.Base64;
-import org.eclipse.orion.server.useradmin.UnsupportedUserStoreException;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.osgi.framework.Version;
@@ -50,47 +48,42 @@ public class FormAuthLoginServlet extends HttpServlet {
 		String pathInfo = req.getPathInfo() == null ? "" : req.getPathInfo(); //$NON-NLS-1$
 
 		if (pathInfo.startsWith("/form")) { //$NON-NLS-1$
-			try {
-				LoginResult authResult = FormAuthHelper.performAuthentication(req, resp);
-				if (authResult == LoginResult.OK) {
-					// redirection from
-					// FormAuthenticationService.setNotAuthenticated
-					String versionString = req.getHeader("Orion-Version"); //$NON-NLS-1$
-					Version version = versionString == null ? null : new Version(versionString);
+			LoginResult authResult = FormAuthHelper.performAuthentication(req, resp);
+			if (authResult == LoginResult.OK) {
+				// redirection from
+				// FormAuthenticationService.setNotAuthenticated
+				String versionString = req.getHeader("Orion-Version"); //$NON-NLS-1$
+				Version version = versionString == null ? null : new Version(versionString);
 
-					// TODO: This is a workaround for calls
-					// that does not include the WebEclipse version header
-					String xRequestedWith = req.getHeader("X-Requested-With"); //$NON-NLS-1$
+				// TODO: This is a workaround for calls
+				// that does not include the WebEclipse version header
+				String xRequestedWith = req.getHeader("X-Requested-With"); //$NON-NLS-1$
 
-					if (version == null && !"XMLHttpRequest".equals(xRequestedWith)) { //$NON-NLS-1$
-						if (req.getParameter("redirect") != null && !req.getParameter("redirect").equals("")) { //$NON-NLS-1$
-							resp.sendRedirect(req.getParameter("redirect"));
-						}
-					} else {
-						resp.setStatus(HttpServletResponse.SC_OK);
-						PrintWriter writer = resp.getWriter();
-						String uid = (String) req.getSession().getAttribute("user");
-						JSONObject userJson;
-						try {
-							userJson = FormAuthHelper.getUserJson(uid, req.getContextPath());
-							writer.print(userJson);
-							resp.setContentType("application/json"); //$NON-NLS-1$
-						} catch (JSONException e) {/* ignore */
-						}
+				if (version == null && !"XMLHttpRequest".equals(xRequestedWith)) { //$NON-NLS-1$
+					if (req.getParameter("redirect") != null && !req.getParameter("redirect").equals("")) { //$NON-NLS-1$
+						resp.sendRedirect(req.getParameter("redirect"));
 					}
-					resp.flushBuffer();
-				} else if (authResult == LoginResult.BLOCKED) {
-					displayError("Your account is not active. Please confirm your email before logging in.", req, resp);
 				} else {
-					displayError("Invalid user or password", req, resp);
+					resp.setStatus(HttpServletResponse.SC_OK);
+					PrintWriter writer = resp.getWriter();
+					String uid = (String) req.getSession().getAttribute("user");
+					JSONObject userJson;
+					try {
+						userJson = FormAuthHelper.getUserJson(uid, req.getContextPath());
+						writer.print(userJson);
+						resp.setContentType("application/json"); //$NON-NLS-1$
+					} catch (JSONException e) {/* ignore */
+					}
 				}
-			} catch (UnsupportedUserStoreException e) {
-				LogHelper.log(e);
-				resp.sendError(HttpServletResponse.SC_NOT_FOUND, e.getMessage());
+				resp.flushBuffer();
+			} else if (authResult == LoginResult.BLOCKED) {
+				displayError("Your account is not active. Please confirm your email before logging in.", req, resp);
+			} else {
+				displayError("Invalid user or password", req, resp);
 			}
 			return;
 		}
-		if (pathInfo.startsWith("/oauth")){
+		if (pathInfo.startsWith("/oauth")) {
 			try {
 				manageOAuthServlet.handleGetAndLogin(req, resp);
 			} catch (OAuthException e) {
@@ -167,7 +160,7 @@ public class FormAuthLoginServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String pathInfo = req.getPathInfo() == null ? "" : req.getPathInfo(); //$NON-NLS-1$
-		if (pathInfo.startsWith("/oauth") ) { //$NON-NLS-1$
+		if (pathInfo.startsWith("/oauth")) { //$NON-NLS-1$
 			doPost(req, resp);
 			return;
 		}
