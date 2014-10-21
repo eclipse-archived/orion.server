93786e3b4b98d8e990a951a4ccd80e62f69bbb8e
Aidan Redpath <aidanr@ca.ibm.com>
2014-10-21 15:15:29 -0400

    Added a basic grep file search
    Change-Id: Idf5a1896aea732d8d8a982d8e9d0d0a5fa775429
    Signed-off-by: Aidan Redpath <aidanr@ca.ibm.com>

---
+++ bundles/org.eclipse.orion.server.search/src/org/eclipse/orion/internal/server/search/grep/GrepServlet.java
@@ -0,0 +1,54 @@
+package org.eclipse.orion.internal.server.search.grep;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.orion.server.servlets.OrionServlet;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class GrepServlet extends OrionServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			FileGrepper grepper = new FileGrepper(req, resp);
+			List<File> files = grepper.search();
+			writeResponse(req, resp, files);
+		} catch (GrepException e) {
+			resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+		}
+	}
+
+	private void writeResponse(HttpServletRequest req, HttpServletResponse resp, List<File> files) {
+		try {
+			JSONObject json = convertListToJson(files);
+			PrintWriter writer = resp.getWriter();
+			writer.write(json.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private JSONObject convertListToJson(List<File> files) {
+		JSONObject json = new JSONObject();
+		LinkedList<String> filenames = new LinkedList<String>();
+		for (File file : files) {
+			filenames.add(file.getPath());
+		}
+		try {
+			json.put("files", filenames);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return json;
+	}
+}
