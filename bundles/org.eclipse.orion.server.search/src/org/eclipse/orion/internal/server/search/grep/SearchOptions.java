36286d0196e09f941bfc22876a56e7f545c34f71
Aidan Redpath <aidanr@ca.ibm.com>
2014-11-06 15:26:39 -0500

    Added file search capabilities
    Modified grep search to do a file search when no search term is provided.
    Changed Grep search to accept and return solr query info
    
    Change-Id: Ie35129af564be382d59596cb80812b06f15c4e45
    Signed-off-by: Aidan Redpath <aidanr@ca.ibm.com>

--- bundles/org.eclipse.orion.server.search/src/org/eclipse/orion/internal/server/search/grep/SearchOptions.java
+++ bundles/org.eclipse.orion.server.search/src/org/eclipse/orion/internal/server/search/grep/SearchOptions.java
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package org.eclipse.orion.internal.server.search.grep;
 
-import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
@@ -22,75 +21,50 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class SearchOptions {
 
-	private static final String[] FIELD_NAMES = {"Name", "Location", "RegEx", "CaseSensitive"}; //$NON-NLS-1$
-
 	private String searchTerm;
-	private String[] fileNames;
+	private String[] filenames;
 
 	private boolean caseSensitive;
 	private boolean regEx;
+	private boolean fileSearch;
 
-	private File scope;
+	private String scope;
+
+	public SearchOptions() {
+	}
 
 	/**
 	 * Creates a search option from the servlet parameters. Builds the options from the request parameter 'q'.
 	 * All the search terms are seperated by a space or plus ('+') character.
 	 * @param req The HTTP request to the servlet.
 	 * @param resp The HTTP response from the servlet.
+	 * @throws GrepException If there is a problem parsing the query data
 	 */
-	public SearchOptions(HttpServletRequest req, HttpServletResponse resp) {
-		String queryString = req.getParameter("q");
-		//divide into search terms delimited by space or plus ('+') character
-		String[] terms = queryString.split("[\\s\\+]+"); //$NON-NLS-1$
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < terms.length; i++) {
-			String term = terms[i];
-			// If it is not a search field it is part of the search term.
-			if (!isSearchField(term)) {
-				try {
-					sb.append(URLDecoder.decode(term, "UTF-8")); //$NON-NLS-1$
-				} catch (UnsupportedEncodingException e) {
+	public SearchOptions(HttpServletRequest req, HttpServletResponse resp) throws GrepException {
+		try {
+			String queryString = req.getParameter("q");
+			if (queryString != null)
+				searchTerm = URLDecoder.decode(queryString, "UTF-8");
 
-				}
-				searchTerm = term;
-				continue;
-			}
-			if (term.startsWith("Name:")) { //$NON-NLS-1$
-				String cvs = getTermValue(term, "Name:");
-				fileNames = cvs.split(",");
-			} else if (term.startsWith("Location:")) { //$NON-NLS-1${
-				String url = getTermValue(term, "Location:");
-				scope = new File(url);
-			} else if (term.startsWith("CaseSensitive:")) { //$NON-NLS-1$
-				caseSensitive = true;
-			} else if (term.startsWith("RegEx:")) { //$NON-NLS-1$
-				regEx = true;
-			}
+			fileSearch = searchTerm != null && !searchTerm.isEmpty();
+
+			queryString = req.getParameter("qf");
+			if (queryString != null)
+				filenames = URLDecoder.decode(queryString, "UTF-8").split(",");
+
+			queryString = req.getParameter("qs");
+			if (queryString != null)
+				scope = URLDecoder.decode(queryString, "UTF-8");
+
+			queryString = req.getParameter("qr");
+			regEx = queryString != null;
+
+			queryString = req.getParameter("qcs");
+			caseSensitive = queryString != null;
+
+		} catch (UnsupportedEncodingException e) {
+			throw new GrepException(e);
 		}
-		searchTerm = sb.toString();
-	}
-
-	/**
-	 * Gets the value from a search term. term='key'+'value'
-	 * @param term The current search term.
-	 * @param key The key of the term.
-	 * @return All the characters after the key in the term.
-	 */
-	private String getTermValue(String term, String key) {
-		return term.substring(key.length());
-	}
-
-	/**
-	 * Checks if the current term is a search term.
-	 * @param term The current search term to check.
-	 * @return True if the current search term is a valid search field.
-	 */
-	private boolean isSearchField(String term) {
-		for (String field : FIELD_NAMES) {
-			if (term.startsWith(field + ":")) //$NON-NLS-1$
-				return true;
-		}
-		return false;
 	}
 
 	/**
@@ -100,11 +74,20 @@ public class SearchOptions {
 		return searchTerm;
 	}
 
+	public void setSearchTerm(String searchTerm) {
+		this.searchTerm = searchTerm;
+		setFileSearch(true);
+	}
+
 	/**
 	 * Returns the list of filename patterns
 	 */
-	public String[] getFileNamePatterns() {
-		return fileNames;
+	public String[] getFilenamePatterns() {
+		return filenames;
+	}
+
+	public void setFilenamePatterns(String[] filenames) {
+		this.filenames = filenames;
 	}
 
 	/**
@@ -114,6 +97,10 @@ public class SearchOptions {
 		return caseSensitive;
 	}
 
+	public void setIsCaseSensitive(boolean caseSensitive) {
+		this.caseSensitive = caseSensitive;
+	}
+
 	/**
 	 * Returns if the search is regex.
 	 */
@@ -121,10 +108,29 @@ public class SearchOptions {
 		return regEx;
 	}
 
+	public void setRegEx(boolean regEx) {
+		this.regEx = regEx;
+	}
+
+	/**
+	 * Returns if the search needs to search the file contents
+	 */
+	public boolean isFileSearch() {
+		return fileSearch;
+	}
+
+	public void setFileSearch(boolean fileSearch) {
+		this.fileSearch = fileSearch;
+	}
+
 	/**
 	 * Returns the scope.
 	 */
-	public File getScope() {
+	public String getScope() {
 		return scope;
+	}
+
+	public void setScope(String scope) {
+		this.scope = scope;
 	}
 }
