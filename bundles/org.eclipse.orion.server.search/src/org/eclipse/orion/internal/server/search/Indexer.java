f8ec78b9686fade87418bee8e15f783709b9bf0e
John Arthorne <John_Arthorne@ca.ibm.com>
2014-11-07 14:56:20 -0500

    Bug 442822 - Only index active users

--- bundles/org.eclipse.orion.server.search/src/org/eclipse/orion/internal/server/search/Indexer.java
+++ bundles/org.eclipse.orion.server.search/src/org/eclipse/orion/internal/server/search/Indexer.java
@@ -10,49 +10,24 @@
  *******************************************************************************/
 package org.eclipse.orion.internal.server.search;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.StringWriter;
+import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.text.Format;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-
-import org.apache.solr.client.solrj.SolrQuery;
-import org.apache.solr.client.solrj.SolrServer;
-import org.apache.solr.client.solrj.SolrServerException;
+import java.util.*;
+import org.apache.solr.client.solrj.*;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.client.solrj.util.ClientUtils;
 import org.apache.solr.common.SolrException;
 import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.common.params.CommonParams;
-import org.eclipse.core.filesystem.EFS;
-import org.eclipse.core.filesystem.IFileInfo;
-import org.eclipse.core.filesystem.IFileStore;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.OperationCanceledException;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.core.filesystem.*;
+import org.eclipse.core.runtime.*;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.orion.internal.server.servlets.Activator;
-import org.eclipse.orion.server.core.IOUtilities;
-import org.eclipse.orion.server.core.OrionConfiguration;
-import org.eclipse.orion.server.core.ProtocolConstants;
-import org.eclipse.orion.server.core.metastore.IMetaStore;
-import org.eclipse.orion.server.core.metastore.ProjectInfo;
-import org.eclipse.orion.server.core.metastore.UserInfo;
-import org.eclipse.orion.server.core.metastore.WorkspaceInfo;
+import org.eclipse.orion.server.core.*;
+import org.eclipse.orion.server.core.metastore.*;
 import org.eclipse.orion.server.core.resources.FileLocker;
 import org.eclipse.orion.server.core.users.UserConstants2;
 import org.eclipse.osgi.util.NLS;
@@ -375,9 +350,10 @@ public class Indexer extends Job {
 			indexed = 0;
 			for (String userId : userIds) {
 				UserInfo userInfo = metaStore.readUser(userId);
-				if (isActiveUser(userInfo))
+				if (isActiveUser(userInfo)) {
 					activeUserCount++;
-				indexed += indexUser(userInfo, progress.newChild(1), documents);
+					indexed += indexUser(userInfo, progress.newChild(1), documents);
+				}
 			}
 		} catch (CoreException e) {
 			handleIndexingFailure(e, null);
@@ -406,7 +382,7 @@ public class Indexer extends Job {
 		if (logger.isInfoEnabled()) {
 			long time = System.currentTimeMillis();
 			Date date = new Date(time + delay);
-			Format format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+			Format format = new SimpleDateFormat("yyyy-MM-dd HH:mm");//$NON-NLS-1$
 			logger.info("Scheduling indexing to start again at " + format.format(date).toString()); //$NON-NLS-1$
 		}
 		schedule(delay);
@@ -414,11 +390,11 @@ public class Indexer extends Job {
 	}
 
 	/**
-	 * Returns whether the given user has logged in recently. This method conservatively <code>true</code>
+	 * Returns whether the given user has logged in recently. This method conservatively returns <code>true</code>
 	 * if there is any failure determining whether the user is active (user is assumed active until proven otherwise).
 	 */
 	private boolean isActiveUser(UserInfo userInfo) {
-		String prop = userInfo.getProperty(UserConstants2.LAST_LOGIN_TIMESTAMP); //$NON-NLS-1$
+		String prop = userInfo.getProperty(UserConstants2.LAST_LOGIN_TIMESTAMP);
 		if (prop == null)
 			return true;
 		try {
