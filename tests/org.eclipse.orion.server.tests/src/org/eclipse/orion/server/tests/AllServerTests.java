44ac9688083a9373bee33f370fd05157bcd79828
Anthony Hunter <anthonyh@ca.ibm.com>
2014-12-04 14:52:32 -0500

    Bug 453891 remove FileChangeNotificationServiceTests from AllServerTests

--- tests/org.eclipse.orion.server.tests/src/org/eclipse/orion/server/tests/AllServerTests.java
+++ tests/org.eclipse.orion.server.tests/src/org/eclipse/orion/server/tests/AllServerTests.java
@@ -11,7 +11,6 @@
 package org.eclipse.orion.server.tests;
 
 import org.eclipse.orion.server.tests.cf.AllCFTests;
-import org.eclipse.orion.server.tests.events.FileChangeNotificationServiceTests;
 import org.eclipse.orion.server.tests.metastore.ProjectInfoTests;
 import org.eclipse.orion.server.tests.metastore.SimpleMetaStoreConcurrencyTests;
 import org.eclipse.orion.server.tests.metastore.SimpleMetaStoreLiveMigrationConcurrencyTests;
@@ -47,7 +46,6 @@ import org.junit.runners.Suite.SuiteClasses;
 		Base64Test.class, //
 		BasicUsersTest.class, //
 		CoreFilesTest.class, //
-		FileChangeNotificationServiceTests.class, //
 		MetaStoreTest.class, //
 		PreferenceTest.class, //
 		ProjectInfoTests.class, //
