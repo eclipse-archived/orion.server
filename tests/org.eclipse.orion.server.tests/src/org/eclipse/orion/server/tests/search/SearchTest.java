63b2379539306dba3f6197f880e37aa6dbdf7f92
Anthony Hunter <anthonyh@ca.ibm.com>
2015-01-28 11:04:33 -0500

    Bug 458450 Add test for find File fails if name contains '-'

--- tests/org.eclipse.orion.server.tests/src/org/eclipse/orion/server/tests/search/SearchTest.java
+++ tests/org.eclipse.orion.server.tests/src/org/eclipse/orion/server/tests/search/SearchTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2014 IBM Corporation and others.
+ * Copyright (c) 2012, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -417,6 +417,9 @@ public class SearchTest extends FileSystemTest {
 		searchResult = doSearch("NameLower:dbcs.txt");
 		assertOneMatch(searchResult, "DBCS.txt");
 
+		// search with lucene escaped dash
+		searchResult = doSearch("Name:dbcs\\-*");
+		assertOneMatch(searchResult, "dbcs-folder.txt");
 	}
 
 }
