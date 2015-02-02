0bd609adb17bc4a111012d5eca088cee1c110b1d
hermansb <ssjherman@gmail.com>
2015-02-02 18:26:24 -0500

    Bug 54345 in IDS Web IDE Project Added Job to reconnect to MQTT Broker every 5 mins while disconnected.
    Signed-off-by: Herman Badwal <ssjherman@gmail.com>

--- bundles/org.eclipse.orion.server.core/src/org/eclipse/orion/server/core/events/IEventService.java
+++ bundles/org.eclipse.orion.server.core/src/org/eclipse/orion/server/core/events/IEventService.java
@@ -13,32 +13,41 @@ package org.eclipse.orion.server.core.events;
 import org.json.JSONObject;
 
 /**
- * Interface for the Orion event service. It has capabilities the publishing of 
- * MQTT events for changes within the Orion server.
- *  
+ * Interface for the Orion event service. It has capabilities the publishing of MQTT events for changes within the Orion server.
+ * 
  * @author Anthony Hunter, Malgorzata Janczarska
  */
 public interface IEventService {
 
 	/**
 	 * Publish an event to the MQTT message broker.
-	 * @param topic The topic for the event.
-	 * @param message JSON describing the event.
+	 * 
+	 * @param topic
+	 *            The topic for the event.
+	 * @param message
+	 *            JSON describing the event.
 	 */
 	public void publish(String topic, JSONObject message);
-	
+
 	/**
 	 * Register a listener that will receive messages from given topic
-	 * @param topic topic The topic for the event.
+	 * 
+	 * @param topic
+	 *            topic The topic for the event.
 	 * @param messageListener
 	 */
 	public void receive(String topic, IMessageListener messageListener);
-	
+
 	/**
 	 * Unregister the listener from the topic
+	 * 
 	 * @param topic
 	 * @param messageListener
 	 */
 	public void stopReceiving(String topic, IMessageListener messageListener);
+
+	void reconnectMQTTClient();
+
+	boolean clientConnected();
 
 }
