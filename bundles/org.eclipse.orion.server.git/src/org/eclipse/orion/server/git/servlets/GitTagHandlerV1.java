e0cbac84e1b877ae4e312b76275246c870a95851
Anthony Hunter <anthonyh@ca.ibm.com>
2015-02-10 17:14:45 -0500

    Bug 459609 GitTagTest.testTagFailed is failing

--- bundles/org.eclipse.orion.server.git/src/org/eclipse/orion/server/git/servlets/GitTagHandlerV1.java
+++ bundles/org.eclipse.orion.server.git/src/org/eclipse/orion/server/git/servlets/GitTagHandlerV1.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2014 IBM Corporation and others.
+ * Copyright (c) 2011, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -116,7 +116,7 @@ public class GitTagHandlerV1 extends AbstractGitHandler {
 			return true;
 		} catch (Exception e) {
 			return statusHandler.handleRequest(request, response, new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-					"An error occured when tagging.", e));
+					"An error occurred when tagging.", e));
 		} finally {
 			walk.dispose();
 		}
