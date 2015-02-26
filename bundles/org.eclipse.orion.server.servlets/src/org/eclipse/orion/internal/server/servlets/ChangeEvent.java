33b08b78160a9240fa0134aa1d1cd33fb6c43593
skaegi <simon_kaegi@ca.ibm.com>
2015-02-25 23:24:19 -0800

    Adding hash/equals to support use in hashmaps and sets

--- bundles/org.eclipse.orion.server.servlets/src/org/eclipse/orion/internal/server/servlets/ChangeEvent.java
+++ bundles/org.eclipse.orion.server.servlets/src/org/eclipse/orion/internal/server/servlets/ChangeEvent.java
@@ -16,7 +16,7 @@ import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.orion.internal.server.servlets.IFileStoreModificationListener.ChangeType;
 
 /**
- * Describes an edit change to a file or folder. 
+ * Describes an edit change to a file or folder.
  */
 public class ChangeEvent extends EventObject {
 	private static final long serialVersionUID = -4402869221399239402L;
@@ -42,17 +42,50 @@ public class ChangeEvent extends EventObject {
 	}
 
 	/**
-	 * Get the item that was changed on disk. 
+	 * Get the item that was changed on disk.
 	 */
 	public IFileStore getModifiedItem() {
 		return modified;
 	}
 
 	/**
-	 * For a move, return the initial location. For a copy, return the initial source. 
-	 * For other change types, the return value is <code>null</code>.
+	 * For a move, return the initial location. For a copy, return the initial source. For other change types, the return value is <code>null</code>.
 	 */
 	public IFileStore getInitialLocation() {
 		return start;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((modified == null) ? 0 : modified.hashCode());
+		result = prime * result + ((start == null) ? 0 : start.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ChangeEvent other = (ChangeEvent) obj;
+		if (modified == null) {
+			if (other.modified != null)
+				return false;
+		} else if (!modified.equals(other.modified))
+			return false;
+		if (start == null) {
+			if (other.start != null)
+				return false;
+		} else if (!start.equals(other.start))
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
 	}
 }
