177446075c5757c560d260716091c6a2ba92038a
Anthony Hunter <anthonyh@ca.ibm.com>
2015-03-04 14:16:36 -0500

    Bug 461431 [build] Fix publish script to use newer Eclipse build

--- releng/org.eclipse.orion.server.repository/orion-hudson-publish.sh
+++ releng/org.eclipse.orion.server.repository/orion-hudson-publish.sh
@@ -79,8 +79,8 @@ echo "Working in `pwd`"
 
 # Download and prepare Eclipse SDK, which is needed to process the update site
 echo "Downloading eclipse to $PWD"
-cp /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4/R-4.4.1-201409250400/eclipse-SDK-4.4.1-linux-gtk-x86_64.tar.gz .
-tar -xzf eclipse-SDK-4.4.1-linux-gtk-x86_64.tar.gz
+cp /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4/R-4.4.2-201502041700/eclipse-SDK-4.4.2-linux-gtk-x86_64.tar.gz .
+tar -xzf eclipse-SDK-4.4.2-linux-gtk-x86_64.tar.gz
 cd eclipse
 chmod 700 eclipse
 cd ..
@@ -93,7 +93,7 @@ echo "Installing WTP Releng tools"
 ./eclipse/eclipse -nosplash --launcher.suppressErrors -clean -debug -application org.eclipse.equinox.p2.director -repository http://download.eclipse.org/webtools/releng/repository/ -installIUs org.eclipse.wtp.releng.tools.feature.feature.group
 # Clean up
 echo "Cleaning up"
-rm eclipse-SDK-4.4.1-linux-gtk-x86_64.tar.gz
+rm eclipse-SDK-4.4.2-linux-gtk-x86_64.tar.gz
 
 # Generate drop files
 qualifiedVersion=$(find $localUpdateSite/features/ -maxdepth 1 | grep "org.eclipse.orion_" | sed 's/.jar$//')
