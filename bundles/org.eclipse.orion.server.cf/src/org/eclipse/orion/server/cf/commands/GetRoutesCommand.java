97d4e59d88ef2ab8521def900f17fb0163dc99fd
Filip Madej <filip.madej@pl.ibm.com>
2015-03-13 13:05:18 +0100

    [Bug 461406] Implement new solution in GetRoutesCommand class to handle CFService#getRoute
    Change-Id: I5d0f670914b0009af6e921efef3c611ea8d44356
    Signed-off-by: Filip Madej <filip.madej@pl.ibm.com>

--- bundles/org.eclipse.orion.server.cf/src/org/eclipse/orion/server/cf/commands/GetRoutesCommand.java
+++ bundles/org.eclipse.orion.server.cf/src/org/eclipse/orion/server/cf/commands/GetRoutesCommand.java
@@ -11,11 +11,16 @@
 package org.eclipse.orion.server.cf.commands;
 
 import java.net.URI;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.httpclient.methods.GetMethod;
-import org.eclipse.core.runtime.*;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.URIUtil;
 import org.eclipse.orion.server.cf.CFProtocolConstants;
 import org.eclipse.orion.server.cf.objects.Route;
 import org.eclipse.orion.server.cf.objects.Target;
@@ -58,22 +63,42 @@ public class GetRoutesCommand extends AbstractCFCommand {
 	@Override
 	protected ServerStatus _doIt() {
 		try {
-			/* get available routes */
+			
 			URI targetURI = URIUtil.toURI(target.getUrl());
-			String routesURL = target.getSpace().getCFJSON().getJSONObject(CFProtocolConstants.V2_KEY_ENTITY).getString(CFProtocolConstants.V2_KEY_ROUTES_URL);
-			URI routesURI = targetURI.resolve(routesURL);
+			ServerStatus getRouteStatus;
+			
+			if(domainName != null && hostName != null){
+				GetDomainsCommand getDomainsCommand = new GetDomainsCommand(target, domainName);
+				ServerStatus getDomainsStatus = (ServerStatus) getDomainsCommand.doIt();
+				if (!getDomainsStatus.isOK())
+					return getDomainsStatus;
+				
+				String domainId = getDomainsCommand.getDomains().get(0).getGuid();
+				
+				URI routesURI = targetURI.resolve("/v2/routes");
+				GetMethod getRouteMethod = new GetMethod(routesURI.toString());
+				HttpUtil.configureHttpMethod(getRouteMethod, target.getCloud());
+				getRouteMethod.setQueryString("inline-relations-depth=1&q=" + URLEncoder.encode(CFProtocolConstants.V2_KEY_HOST + ":" + hostName + ";" + CFProtocolConstants.V2_KEY_DOMAIN_GUID + ":" + domainId, "UTF8"));
 
-			GetMethod getRoutesMethod = new GetMethod(routesURI.toString());
-			HttpUtil.configureHttpMethod(getRoutesMethod, target.getCloud());
-			getRoutesMethod.setQueryString("inline-relations-depth=1"); //$NON-NLS-1$
+				getRouteStatus = HttpUtil.executeMethod(getRouteMethod);
+				if (!getRouteStatus.isOK())
+					return getRouteStatus;
 
-			ServerStatus getRouteStatus = HttpUtil.executeMethod(getRoutesMethod);
-			if (!getRouteStatus.isOK())
-				return getRouteStatus;
+			} else {
+				String routesURL = target.getSpace().getCFJSON().getJSONObject(CFProtocolConstants.V2_KEY_ENTITY).getString(CFProtocolConstants.V2_KEY_ROUTES_URL);
+				
+				URI routesURI = targetURI.resolve(routesURL);
+				GetMethod getRoutesMethod = new GetMethod(routesURI.toString());
+				HttpUtil.configureHttpMethod(getRoutesMethod, target.getCloud());
+				getRoutesMethod.setQueryString("inline-relations-depth=1"); //$NON-NLS-1$
 
-			/* extract available routes */
+				getRouteStatus = HttpUtil.executeMethod(getRoutesMethod);
+				if (!getRouteStatus.isOK())
+					return getRouteStatus;
+
+			}
+		
 			JSONObject routesJSON = getRouteStatus.getJsonData();
-
 			if (routesJSON.getInt(CFProtocolConstants.V2_KEY_TOTAL_RESULTS) < 1) {
 				return new ServerStatus(IStatus.OK, HttpServletResponse.SC_OK, null, null);
 			}
@@ -86,14 +111,11 @@ public class GetRoutesCommand extends AbstractCFCommand {
 				Route route = new Route();
 				route.setCFJSON(routeJSON);
 
-				if (domainName == null || (domainName.equals(route.getDomainName()) && (hostName == null || hostName.equals(route.getHost())))) {
-					if (!orphaned || route.getCFJSON().getJSONObject(CFProtocolConstants.V2_KEY_ENTITY).getJSONArray(CFProtocolConstants.V2_KEY_APPS).length() == 0) {
-						routes.add(route);
-						result.append("Routes", route.toJSON());
-					}
-				}
+				if (!orphaned || route.getCFJSON().getJSONObject(CFProtocolConstants.V2_KEY_ENTITY).getJSONArray(CFProtocolConstants.V2_KEY_APPS).length() == 0) {
+					routes.add(route);
+					result.append("Routes", route.toJSON());
+				}	
 			}
-
 			return new ServerStatus(Status.OK_STATUS, HttpServletResponse.SC_OK, result);
 		} catch (Exception e) {
 			String msg = NLS.bind("An error occured when performing operation {0}", commandName); //$NON-NLS-1$
