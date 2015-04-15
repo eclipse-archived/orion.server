536cc154d44808e774cc73dd62b4ba1546d11966
John Arthorne <John_Arthorne@ca.ibm.com>
2015-04-15 09:59:15 -0400

    Bug 464692 - Instrument Git status performance

--- bundles/org.eclipse.orion.server.git/src/org/eclipse/orion/server/git/servlets/GitStatusHandlerV1.java
+++ bundles/org.eclipse.orion.server.git/src/org/eclipse/orion/server/git/servlets/GitStatusHandlerV1.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2014 IBM Corporation and others.
+ * Copyright (c) 2011, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -33,12 +33,15 @@ import org.eclipse.orion.server.git.servlets.GitUtils.Traverse;
 import org.eclipse.orion.server.servlets.JsonURIUnqualificationStrategy;
 import org.eclipse.orion.server.servlets.OrionServlet;
 import org.eclipse.osgi.util.NLS;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A handler for Git Status operation.
  */
 public class GitStatusHandlerV1 extends ServletResourceHandler<String> {
 
+	private static final int GIT_PERF_THRESHOLD = 10000;// 10 seconds
 	private ServletResourceHandler<IStatus> statusHandler;
 
 	GitStatusHandlerV1(ServletResourceHandler<IStatus> statusHandler) {
@@ -47,6 +50,7 @@ public class GitStatusHandlerV1 extends ServletResourceHandler<String> {
 
 	@Override
 	public boolean handleRequest(HttpServletRequest request, HttpServletResponse response, String gitPathInfo) throws ServletException {
+		Logger logger = LoggerFactory.getLogger("org.eclipse.orion.server.git");
 		Repository db = null;
 		try {
 			Path path = new Path(gitPathInfo);
@@ -58,24 +62,33 @@ public class GitStatusHandlerV1 extends ServletResourceHandler<String> {
 				response.sendError(HttpServletResponse.SC_FORBIDDEN);
 				return true;
 			}
+
+			long t0 = System.currentTimeMillis();
 			Set<Entry<IPath, File>> set = GitUtils.getGitDirs(path, Traverse.GO_UP).entrySet();
 			File gitDir = set.iterator().next().getValue();
-			if (gitDir == null)
+			if (gitDir == null) {
+				logger.error("***** Git status failed to find Git directory for request: " + path);
 				return false; // TODO: or an error response code, 405?
+			}
+			long t1 = System.currentTimeMillis();
 			db = FileRepositoryBuilder.create(gitDir);
 			Git git = new Git(db);
 			org.eclipse.jgit.api.Status gitStatus = git.status().call();
+			long t2 = System.currentTimeMillis();
 
 			URI baseLocation = getURI(request);
 			String relativePath = GitUtils.getRelativePath(path, set.iterator().next().getKey());
 			IPath basePath = new Path(relativePath);
 			Status status = new Status(baseLocation, db, gitStatus, basePath);
 			OrionServlet.writeJSONResponse(request, response, status.toJSON(), JsonURIUnqualificationStrategy.ALL_NO_GIT);
+			if (logger.isDebugEnabled() && (t2 - t0) > GIT_PERF_THRESHOLD) {
+				logger.debug("Slow git status. Finding git dir: " + (t1 - t0) + "ms. JGit status call: " + (t2 - t1) + "ms");
+			}
 			return true;
 
 		} catch (Exception e) {
-			return statusHandler.handleRequest(request, response, new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-					"Error generating status response", e));
+			return statusHandler.handleRequest(request, response,
+					new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error generating status response", e));
 		} finally {
 			if (db != null) {
 				// close the git repository
