9cab295cf7923595d77f5d4d2a6ec758a7e3d0a3
Anthony Hunter <anthonyh@ca.ibm.com>
2015-05-12 17:55:56 -0400

    [nobug] Revert additional logging to see why sites are throwing an error 404 caused by second encoding of already encoded path server side.

--- bundles/org.eclipse.orion.server.hosting/src/org/eclipse/orion/server/hosting/HostedSiteRequestFilter.java
+++ bundles/org.eclipse.orion.server.hosting/src/org/eclipse/orion/server/hosting/HostedSiteRequestFilter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2015 IBM Corporation and others.
+ * Copyright (c) 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,20 +10,11 @@
  *******************************************************************************/
 package org.eclipse.orion.server.hosting;
 
+import org.eclipse.orion.internal.server.hosting.*;
+
 import java.io.IOException;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
-
-import org.eclipse.orion.internal.server.hosting.HostingActivator;
-import org.eclipse.orion.internal.server.hosting.HostingConstants;
-import org.eclipse.orion.internal.server.hosting.ISiteHostingService;
 
 /**
  * If an incoming request is for a path on a running hosted site (based on Host header), 
