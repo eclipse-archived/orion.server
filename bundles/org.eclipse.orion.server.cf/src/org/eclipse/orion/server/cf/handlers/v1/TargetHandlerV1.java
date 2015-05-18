a4a6aa64603e6e090f06894dbd5539f56a050852
Szymon Brandys <Szymon.Brandys@pl.ibm.com>
2015-05-18 15:56:16 +0200

    [nobug] API to initialize target registry

--- bundles/org.eclipse.orion.server.cf/src/org/eclipse/orion/server/cf/handlers/v1/TargetHandlerV1.java
+++ bundles/org.eclipse.orion.server.cf/src/org/eclipse/orion/server/cf/handlers/v1/TargetHandlerV1.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2014 IBM Corporation and others 
+ * Copyright (c) 2013, 2015 IBM Corporation and others 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,19 +10,28 @@
  *******************************************************************************/
 package org.eclipse.orion.server.cf.handlers.v1;
 
+import java.net.URL;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.eclipse.core.runtime.*;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.orion.internal.server.servlets.ServletResourceHandler;
 import org.eclipse.orion.server.cf.CFActivator;
 import org.eclipse.orion.server.cf.CFProtocolConstants;
-import org.eclipse.orion.server.cf.commands.*;
+import org.eclipse.orion.server.cf.commands.ComputeTargetCommand;
+import org.eclipse.orion.server.cf.commands.LoginCommand;
+import org.eclipse.orion.server.cf.commands.LogoutCommand;
 import org.eclipse.orion.server.cf.jobs.CFJob;
+import org.eclipse.orion.server.cf.objects.Cloud;
 import org.eclipse.orion.server.cf.objects.Target;
 import org.eclipse.orion.server.cf.servlets.AbstractRESTHandler;
 import org.eclipse.orion.server.core.IOUtilities;
 import org.eclipse.orion.server.core.ServerStatus;
 import org.eclipse.osgi.util.NLS;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -85,6 +94,33 @@ public class TargetHandlerV1 extends AbstractRESTHandler<Target> {
 					CFActivator.getDefault().getTargetRegistry().setDefaultTarget(this.userId, target);
 
 					return new ServerStatus(Status.OK_STATUS, HttpServletResponse.SC_OK, target.toJSON());
+				} catch (Exception e) {
+					String msg = NLS.bind("Failed to handle request for {0}", path); //$NON-NLS-1$
+					ServerStatus status = new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, msg, e);
+					logger.error(msg, e);
+					return status;
+				}
+			}
+		};
+	}
+	
+	@Override
+	protected CFJob handlePut(Target resource, HttpServletRequest request, HttpServletResponse response, final String path) {
+		final JSONObject jsonData = extractJSONData(request);
+		return new CFJob(request, false) {
+			@Override
+			protected IStatus performJob() {
+				try {
+					JSONArray regions = jsonData.getJSONArray("Targets");
+					for (int i=0; i < regions.length(); i++){
+						JSONObject region = regions.getJSONObject(i);
+						String url = region.getString("Url");
+						Cloud cloud = CFActivator.getDefault().getTargetRegistry().getCloud(this.userId, new URL(url));
+						JSONObject accessToken = new JSONObject();
+						accessToken.put("access_token", region.getString("AccessToken"));
+						cloud.setAccessToken(accessToken);
+					}
+					return new ServerStatus(Status.OK_STATUS, HttpServletResponse.SC_OK);
 				} catch (Exception e) {
 					String msg = NLS.bind("Failed to handle request for {0}", path); //$NON-NLS-1$
 					ServerStatus status = new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, msg, e);
