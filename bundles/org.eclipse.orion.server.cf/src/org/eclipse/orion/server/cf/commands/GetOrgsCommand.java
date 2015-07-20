590b960ab5fa0be792254fea4f5e61c72dda0c1d
Szymon Brandys <Szymon.Brandys@pl.ibm.com>
2015-07-20 17:41:18 +0200

    Bug 473091 - Add caching mechanism for getting orgs/spaces

--- bundles/org.eclipse.orion.server.cf/src/org/eclipse/orion/server/cf/commands/GetOrgsCommand.java
+++ bundles/org.eclipse.orion.server.cf/src/org/eclipse/orion/server/cf/commands/GetOrgsCommand.java
@@ -12,17 +12,23 @@ package org.eclipse.orion.server.cf.commands;
 
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.httpclient.ConnectTimeoutException;
 import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.URIUtil;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.orion.server.cf.CFProtocolConstants;
+import org.eclipse.orion.server.cf.ExpiryCache;
 import org.eclipse.orion.server.cf.objects.OrgWithSpaces;
 import org.eclipse.orion.server.cf.objects.Space;
 import org.eclipse.orion.server.cf.objects.Target;
@@ -35,16 +41,48 @@ import org.slf4j.LoggerFactory;
 
 public class GetOrgsCommand extends AbstractCFCommand {
 	private final Logger logger = LoggerFactory.getLogger("org.eclipse.orion.server.cf"); //$NON-NLS-1$
+	private static final int CACHE_EXPIRES_MS = 300000;
+	private static final int MAX_CACHE_SIZE = 1000;
 
 	private String commandName;
+	private String userId;
+	static ExpiryCache<JSONObject> orgsCache = new ExpiryCache<JSONObject>(MAX_CACHE_SIZE, CACHE_EXPIRES_MS);
+	static Set<List<Object>> check = new HashSet<List<Object>>();
 
 	public GetOrgsCommand(String userId, Target target) {
 		super(target);
+		this.userId = userId;
 		this.commandName = "Get Spaces";
 	}
 
 	@Override
 	protected ServerStatus _doIt() {
+		final List<Object> key = Arrays.asList(target, userId);
+		final JSONObject cachedOrgs = orgsCache.get(key);
+		if (cachedOrgs != null) {
+			if (!check.contains(key)) {
+				logger.debug(NLS.bind("Need to validate {1} cache for user {0}", userId, target.getUrl().toString()));
+				new Job("Validating cache") {
+					@Override
+					protected IStatus run(IProgressMonitor monitor) {
+						logger.debug(NLS.bind("Validating {1} cache for user {0}", userId, target.getUrl().toString()));
+						check.add(key);
+						ServerStatus getOrgsSatus = getOrgs();
+						check.remove(key);
+						logger.debug(NLS.bind("Validation for {1} cache for user {0} finished", userId, target.getUrl().toString()));
+						return getOrgsSatus;
+					}
+				}.schedule();
+			}
+			
+			logger.debug(NLS.bind("Geting orgs from {1} cache for user {0}", userId, target.getUrl().toString()));
+			return new ServerStatus(Status.OK_STATUS, HttpServletResponse.SC_OK, cachedOrgs);
+		}
+
+		return getOrgs();
+	}
+
+	private ServerStatus getOrgs() {
 		try {
 			/* get available orgs */
 			URI targetURI = URIUtil.toURI(target.getUrl());
@@ -54,10 +92,10 @@ public class GetOrgsCommand extends AbstractCFCommand {
 			ServerStatus confStatus = HttpUtil.configureHttpMethod(getOrgsMethod, target.getCloud());
 			if (!confStatus.isOK())
 				return confStatus;
-			
+
 			List<NameValuePair> params = new ArrayList<NameValuePair>();
 			params.add(new NameValuePair("inline-relations-depth", "1"));
-			if (target.getCloud().getRegion() != null){
+			if (target.getCloud().getRegion() != null) {
 				params.add(new NameValuePair("region", target.getCloud().getRegion()));
 			}
 			getOrgsMethod.setQueryString(params.toArray(new NameValuePair[params.size()]));
@@ -78,17 +116,20 @@ public class GetOrgsCommand extends AbstractCFCommand {
 			int resources = orgs.getJSONArray(CFProtocolConstants.V2_KEY_RESOURCES).length();
 			for (int k = 0; k < resources; ++k) {
 				JSONObject orgJSON = orgs.getJSONArray(CFProtocolConstants.V2_KEY_RESOURCES).getJSONObject(k);
-				
+
 				List<Space> spaces = new ArrayList<Space>();
 				ServerStatus getSpacesStatus = getSpaces(spaces, orgJSON);
 				if (!getSpacesStatus.isOK())
 					return getSpacesStatus;
-				
+
 				OrgWithSpaces orgWithSpaces = new OrgWithSpaces();
 				orgWithSpaces.setCFJSON(orgJSON);
 				orgWithSpaces.setSpaces(spaces);
 				result.append("Orgs", orgWithSpaces.toJSON());
 			}
+
+			List<Object> key = Arrays.asList(target, userId);
+			orgsCache.put(key, result);
 
 			return new ServerStatus(Status.OK_STATUS, HttpServletResponse.SC_OK, result);
 		} catch (ConnectTimeoutException e) {
@@ -111,7 +152,7 @@ public class GetOrgsCommand extends AbstractCFCommand {
 		ServerStatus confStatus = HttpUtil.configureHttpMethod(getDomainsMethod, target.getCloud());
 		if (!confStatus.isOK())
 			return confStatus;
-		
+
 		getDomainsMethod.setQueryString("inline-relations-depth=1"); //$NON-NLS-1$
 
 		ServerStatus status = HttpUtil.executeMethod(getDomainsMethod);
