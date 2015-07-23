d45ed98af302c5fdbeda482679918dc4bfff0fe9
Anthony Hunter <anthonyh@ca.ibm.com>
2015-07-23 16:57:01 -0400

    Bug 470394 Fatal error. Server metastore is unavailable!

--- bundles/org.eclipse.orion.server.authentication/src/org/eclipse/orion/server/authentication/Activator.java
+++ bundles/org.eclipse.orion.server.authentication/src/org/eclipse/orion/server/authentication/Activator.java
@@ -13,6 +13,11 @@ package org.eclipse.orion.server.authentication;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.orion.server.core.LogHelper;
 import org.eclipse.orion.server.core.OrionConfiguration;
 import org.eclipse.orion.server.core.metastore.IMetaStore;
@@ -27,6 +32,8 @@ public class Activator implements BundleActivator {
 	 * The symbolic id of this bundle.
 	 */
 	public static final String PI_AUTHENTICATION_SERVLETS = "org.eclipse.orion.server.authentication"; //$NON-NLS-1$
+
+	private static final Logger logger = LoggerFactory.getLogger(LogHelper.LOGGER_ID);
 
 	private static volatile BundleContext bundleContext;
 
@@ -43,22 +50,47 @@ public class Activator implements BundleActivator {
 	public void start(BundleContext context) throws Exception {
 		singleton = this;
 		bundleContext = context;
-		Logger logger = LoggerFactory.getLogger(LogHelper.LOGGER_ID); // $NON-NLS-1$
-		IMetaStore metastore = OrionConfiguration.getMetaStore();
-		if (metastore == null) {
-			String msg = "Fatal error starting authentication service. Server metastore is unavailable!";
-			logger.error(msg);
-			throw new RuntimeException(msg);
-		}
-		List<String> keys = new ArrayList<String>();
-		keys.add(UserConstants.EMAIL);
-		keys.add(UserConstants.OAUTH);
-		keys.add(UserConstants.OPENID);
-		metastore.registerUserProperties(keys);
-		logger.debug("Started orion server authentication."); //$NON-NLS-1$
+
+		AuthenticationMetaStoreJob job = new AuthenticationMetaStoreJob();
+		job.schedule();
 	}
 
 	public void stop(BundleContext context) throws Exception {
 		bundleContext = null;
+	}
+
+	class AuthenticationMetaStoreJob extends Job {
+
+		/**
+		 * @param name
+		 */
+		public AuthenticationMetaStoreJob() {
+			super("AuthenticationMetaStoreJob");
+		}
+
+		@Override
+		protected IStatus run(IProgressMonitor monitor) {
+			try {
+				IMetaStore metastore = OrionConfiguration.getMetaStore();
+				if (metastore == null) {
+					String msg = "Error starting Orion authentication service. Server metastore is unavailable! Checking again in 5 seconds";
+					logger.error(msg);
+					schedule(5000);
+					return Status.OK_STATUS;
+				}
+				// register property keys for user caches for user lookups.
+				List<String> keys = new ArrayList<String>();
+				keys.add(UserConstants.EMAIL);
+				keys.add(UserConstants.OAUTH);
+				keys.add(UserConstants.OPENID);
+				metastore.registerUserProperties(keys);
+				logger.info("Started Orion server authentication successfully."); //$NON-NLS-1$
+				return Status.OK_STATUS;
+			} catch (CoreException e) {
+				String msg = "Error starting Orion server authentication service.";
+				logger.error(msg, e);
+				return new Status(Status.ERROR, PI_AUTHENTICATION_SERVLETS, msg, e);
+			}
+		}
 	}
 }
