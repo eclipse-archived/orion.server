8e32f140bcc8e7697103daeb351e70b2af5cfef9
Wojciech Sudol <wojciech.sudol@pl.ibm.com>
2015-11-25 10:55:14 +0100

    Bug 482984 - Separate authorization from LogServlet

--- bundles/org.eclipse.orion.server.logs/src/org/eclipse/orion/server/logs/servlets/LogServlet.java
+++ bundles/org.eclipse.orion.server.logs/src/org/eclipse/orion/server/logs/servlets/LogServlet.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2014 IBM Corporation and others.
+ * Copyright (c) 2013, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,59 +12,29 @@
 package org.eclipse.orion.server.logs.servlets;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.orion.internal.server.servlets.ServletResourceHandler;
-import org.eclipse.orion.server.core.PreferenceHelper;
-import org.eclipse.orion.server.core.ServerConstants;
 import org.eclipse.orion.server.servlets.OrionServlet;
 
 public class LogServlet extends OrionServlet {
 	public static final String LOGAPI_URI = "/logapi"; //$NON-NLS-1$
 
 	private static final long serialVersionUID = 1L;
-	private List<String> authorizedUsers;
 	private ServletResourceHandler<String> logHandler;
 
 	@Override
 	public void init() throws ServletException {
 		logHandler = new LogHandler(getStatusHandler());
-		String users = PreferenceHelper.getString(ServerConstants.CONFIG_AUTH_LOG_SERVICE, null);
-		if (users != null) {
-			authorizedUsers = new ArrayList<String>();
-			authorizedUsers.addAll(Arrays.asList(users.split(","))); //$NON-NLS-1$
-		} else {
-			// TODO: remove workaround when orion.auth.log.service property is published and AuthorizedUserFilter capability updated
-			users = "ahunterhunter,anthonyh,mbendkowski,sbrandys,johna";
-			authorizedUsers = new ArrayList<String>();
-			authorizedUsers.addAll(Arrays.asList(users.split(","))); //$NON-NLS-1$
-			
-		}
 	}
 
 	@Override
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
-		if (authorizedUsers == null) {
-			// no users are authorized to use the log service
-			response.sendError(HttpServletResponse.SC_FORBIDDEN);
-			return;
-		}
-		
-		String login = request.getRemoteUser();
-		if (login == null || ! authorizedUsers.contains(login)) {
-			// the user is not authorized to use the log service
-			response.sendError(HttpServletResponse.SC_FORBIDDEN);
-			return;
-		}
-		
 		String pathInfo = request.getPathInfo();
 		if (logHandler.handleRequest(request, response, pathInfo))
 			return;
